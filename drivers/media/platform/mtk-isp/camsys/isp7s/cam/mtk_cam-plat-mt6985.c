a7c0a22007bd5e40834ec0f0ee0a4a9376c0cd8c
Allan Yang <allan.yang@mediatek.com>
2023-01-03 01:45:51 +0800

    [ALPS07032762] media: platform: mtk-isp7s refactor for v4l2 layer
    1. refine larb
    2. refine raw pipeline num
    3. refine buffer pool
    4. add job layer
    
    MTK-Commit-Id: 7afe6d6e15cb658b2050692dd56bb27106d4a1cf
    
    CR-Id: ALPS07032762
    Feature: [Android Default] Camera Application Basic Functions
    Change-Id: I5d3e42fc29c76c48546ef0d7bc8bd2947cc89dac
    Signed-off-by: Allan Yang <allan.yang@mediatek.com>

--- drivers/media/platform/mtk-isp/camsys/isp7s/cam/mtk_cam-plat-mt6985.c
+++ drivers/media/platform/mtk-isp/camsys/isp7s/cam/mtk_cam-plat-mt6985.c
@@ -2,17 +2,54 @@
 //
 // Copyright (c) 2019 MediaTek Inc.
 
+#include <linux/align.h>
+#include <linux/sizes.h>
 #include <linux/types.h>
 
 #include "mtk_cam-plat.h"
+#include "mtk_cam-meta-mt6983.h"
 
-static const struct plat_data_meta mt6985_plat_meta = {
-	.get_version = NULL,
-	.get_size = NULL,
+#define RAW_STATS_CFG_SIZE \
+	ALIGN(sizeof(struct mtk_cam_uapi_meta_raw_stats_cfg), SZ_1K)
+
+/* meta out max size include 1k meta info and dma buffer size */
+#define RAW_STATS_0_SIZE \
+	ALIGN(ALIGN(sizeof(struct mtk_cam_uapi_meta_raw_stats_0), SZ_1K) + \
+	      MTK_CAM_UAPI_AAO_MAX_BUF_SIZE + MTK_CAM_UAPI_AAHO_MAX_BUF_SIZE + \
+	      MTK_CAM_UAPI_LTMSO_SIZE + \
+	      MTK_CAM_UAPI_FLK_MAX_BUF_SIZE + \
+	      MTK_CAM_UAPI_TSFSO_SIZE * 2 + /* r1 & r2 */ \
+	      MTK_CAM_UAPI_TNCSYO_SIZE \
+	      , (4 * SZ_1K))
+
+#define RAW_STATS_1_SIZE \
+	ALIGN(ALIGN(sizeof(struct mtk_cam_uapi_meta_raw_stats_1), SZ_1K) + \
+	      MTK_CAM_UAPI_AFO_MAX_BUF_SIZE, (4 * SZ_1K))
+
+#define RAW_STATS_2_SIZE \
+	ALIGN(ALIGN(sizeof(struct mtk_cam_uapi_meta_raw_stats_2), SZ_1K) + \
+	      MTK_CAM_UAPI_ACTSO_SIZE, (4 * SZ_1K))
+
+#define SV_STATS_0_SIZE \
+	sizeof(struct mtk_cam_uapi_meta_camsv_stats_0)
+
+static const struct plat_v4l2_data mt6985_v4l2_data = {
+	.raw_pipeline_num = 3,
+	.camsv_pipeline_num = 0,
+	.mraw_pipeline_num = 0,
+
+	.meta_major = MTK_CAM_META_VERSION_MAJOR,
+	.meta_minor = MTK_CAM_META_VERSION_MINOR,
+
+	.meta_cfg_size = RAW_STATS_CFG_SIZE,
+	.meta_stats0_size = RAW_STATS_0_SIZE,
+	.meta_stats1_size = RAW_STATS_1_SIZE,
+	.meta_stats2_size = RAW_STATS_2_SIZE,
+	.meta_sv_ext_size = SV_STATS_0_SIZE,
 };
 
 struct camsys_platform_data mt6985_data = {
 	.platform = "mt6985",
-	.meta = &mt6985_plat_meta,
+	.v4l2 = &mt6985_v4l2_data,
 	.hw = NULL,
 };
