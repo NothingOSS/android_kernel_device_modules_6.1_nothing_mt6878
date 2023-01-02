8d389cf2e4606f0ef3f4dac8d983f008858ea070
Miles Chen <miles.chen@mediatek.com>
2023-01-03 07:15:08 +0800

    [ALPS07673475] drm: fix drm/drm_dp_helper.h build breaks
    commit 5b529e8d9c38 ("drm/dp: Move public DisplayPort headers into dp/")
    moves drm/drm_dp_helper.h to drm/dp/drm_dp_helper.h, fix them.
    
    errors:
    drivers/gpu/drm/mediatek/mediatek_v2/mtk_dp.c:32:10: fatal error: drm/drm_dp_helper.h file not found
    drivers/gpu/drm/mediatek/mediatek_v2/mtk_dp_common.h:10:10: fatal error: drm/drm_dp_helper.h file not found
    drivers/gpu/drm/mediatek/mediatek_v2/mtk_dp_common.h:10:10: fatal error: drm/drm_dp_helper.h file not found
    drivers/gpu/drm/mediatek/mediatek_v2/mtk_dp_common.h:10:10: fatal error: drm/drm_dp_helper.h file not found
    
    MTK-Commit-Id: cb20debba409e3634e8de5ccf51708433ad3bb7b
    
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS07673475
    Feature: Kernel SI Operation
    Change-Id: I319ddf4b88378d3c38aa118c79a67a4791182b7b

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_dp.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_dp.c
@@ -29,7 +29,7 @@
 #include <drm/drm_crtc.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_edid.h>
-#include <drm/drm_dp_helper.h>
+#include <drm/dp/drm_dp_helper.h>
 #include <drm/mediatek_drm.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_modes.h>
