adbad13369002d1e1ac37fb636fc8f453252ca2b
Miles Chen <miles.chen@mediatek.com>
2023-01-03 07:15:54 +0800

    [ALPS07673475] eas: fix cpu_util_cfs argument
    commit: 82762d2af31a ("sched/fair: Replace CFS internal cpu_util()
    with cpu_util_cfs()") replace cpu_util_cfs(rq) with cpu_util_cfs(cpu).
    Change it accordingly to fix the following build breaks:
    
    Errors:
    drivers/misc/mediatek/sched/eas/eas_plus.c:871:34: error: incompatible pointer to integer conversion passing struct rq * to parameter of type int [-Werror,-Wint-conversion]
    drivers/misc/mediatek/sched/sugov/cpufreq_sugov_main.c:331:40: error: incompatible pointer to integer conversion passing struct rq * to parameter of type int [-Werror,-Wint-conversion]
    
    MTK-Commit-Id: d8e4d1d8b2ce90ffb2e5755dd43dd1ce9356532c
    
    Signed-off-by: Miles Chen <miles.chen@mediatek.com>
    CR-Id: ALPS07673475
    Feature: Kernel SI Operation
    Change-Id: I1f1400d56512882b166c65963afa51e4536cbd77

--- drivers/misc/mediatek/sched/eas/eas_plus.c
+++ drivers/misc/mediatek/sched/eas/eas_plus.c
@@ -864,11 +864,10 @@ static inline bool mtk_rt_task_fits_capacity(struct task_struct *p, int cpu,
 static inline unsigned int mtk_task_cap(struct task_struct *p, int cpu,
 					unsigned long min_cap, unsigned long max_cap)
 {
-	struct rq *rq = cpu_rq(cpu);
 	unsigned long cpu_cap = arch_scale_cpu_capacity(cpu);
 	struct util_rq util_rq;
 
-	util_rq.util_cfs = cpu_util_cfs(rq);
+	util_rq.util_cfs = cpu_util_cfs(cpu);
 	util_rq.base = 1;
 
 	return  mtk_cpu_util(cpu, &util_rq, cpu_cap, FREQUENCY_UTIL, p, min_cap, max_cap);
