b0624e015ab4b4337c3d4372903204227bc402e4
Claude Yen <claude.yen@mediatek.com>
2023-01-02 22:22:31 +0800

    [ALPS06397766] lpm: porting lpm driver
    1. sync lpm driver from kernel-5.10
    2. only keep mt6983 KO
    
    MTK-Commit-Id: 61e570cf790d38deb642afaee50f8d7c614bf149
    
    Signed-off-by: Claude Yen <claude.yen@mediatek.com>
    CR-Id: ALPS06397766
    Feature: Suspend
    Change-Id: I668d24ea913812d8a94cc504e35528120a76c070

--- drivers/misc/mediatek/lpm/modules/include/lpm_dbg_cpc_v5.h
+++ drivers/misc/mediatek/lpm/modules/include/lpm_dbg_cpc_v5.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2019 MediaTek Inc.
+ * Copyright (c) 2021 MediaTek Inc.
  */
 
 #ifndef __MTK_PLAT_CPC_REG_H__
@@ -8,26 +8,26 @@
 
 /**************************************
  * CPC related register
- * base address = 0x0C53A000
+ * base address = 0x0C040000
  **************************************/
-#define CPC_FLOW_CTRL_CFG              (0x814)
-#define CPC_MCUSYS_LAST_CORE_REQ       (0x818)
-#define CPC_CPUSYS_LAST_CORE_RESP      (0x81C)
-#define CPC_MCUSYS_LAST_CORE_RESP      (0x824)
-#define CPC_PWR_ON_MASK                (0x828)
-#define CPC_SPMC_PWR_STATUS            (0x840)
-#define CPC_CPUSYS_CPU_ON_SW_HINT_CLR  (0x8AC)
+#define CPC_FLOW_CTRL_CFG              (0x114)
+#define CPC_MCUSYS_LAST_CORE_REQ       (0x118)
+#define CPC_CPUSYS_LAST_CORE_RESP      (0x11C)
+#define CPC_MCUSYS_LAST_CORE_RESP      (0x124)
+#define CPC_PWR_ON_MASK                (0x128)
+#define CPC_SPMC_PWR_STATUS            (0x140)
+#define CPC_CPUSYS_CPU_ON_SW_HINT_CLR  (0x1AC)
 
-#define SPMC_DBG_SETTING               (0xB00)
+#define SPMC_DBG_SETTING               (0x200)
 
-#define CPC_TRACE_DATA_SEL             (0xB14)
+#define CPC_TRACE_DATA_SEL             (0x214)
 
-#define CPC_CPU0_LATENCY               (0xB40)
-#define CPC_CLUSTER_OFF_LATENCY        (0xB60)
-#define CPC_CLUSTER_ON_LATENCY         (0xB64)
-#define CPC_MCUSYS_LATENCY             (0xB68)
-#define CPC_DORMANT_COUNTER            (0xB70)
-#define CPC_DORMANT_COUNTER_CLR        (0xB74)
+#define CPC_CPU0_LATENCY               (0x240)
+#define CPC_CLUSTER_OFF_LATENCY        (0x260)
+#define CPC_CLUSTER_ON_LATENCY         (0x264)
+#define CPC_MCUSYS_LATENCY             (0x268)
+#define CPC_DORMANT_COUNTER            (0x270)
+#define CPC_DORMANT_COUNTER_CLR        (0x274)
 #define CPC_CPU_LATENCY(cpu)           ((CPC_CPU0_LATENCY) + 4 * (cpu))
 
 /**************************************
@@ -46,6 +46,5 @@
 #define SYSRAM_RECENT_MCUSYS_CNT       (0x1D4)
 #define SYSRAM_RECENT_CNT_TS_L         (0x1D8)
 #define SYSRAM_RECENT_CNT_TS_H         (0x1DC)
-
 
 #endif
