b771848c8781960205a24fa0acdf0131cef2afde
mtk12569 <shih-fang.chuang@mediatek.com>
2023-02-07 09:33:57 +0800

    [ALPS07674627] imgsys: CG porting
    [Detail]
    1.Porting ponsot imgsys cg
    
    MTK-Commit-Id: b80c00ab10d00aabf75ad875b98f348718288312
    
    CR-Id: ALPS07674627
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: mtk12569 <shih-fang.chuang@mediatek.com>
    Change-Id: Ifbbcd88619c7cf2738f98c3be5aaefcf0e85351f

--- drivers/media/platform/mtk-isp/imgsys/platforms/isp7sp/mtk_imgsys-plat.h
+++ drivers/media/platform/mtk-isp/imgsys/platforms/isp7sp/mtk_imgsys-plat.h
@@ -19,9 +19,6 @@ struct clk_bulk_data imgsys_isp7_clks[] = {
 		.id = "IMGSYS_CG_IMG_TRAW1",
 	},
 	{
-		.id = "IMGSYS_CG_IMG_VCORE_GALS",
-	},
-	{
 		.id = "IMGSYS_CG_IMG_DIP0",
 	},
 	{
@@ -40,10 +37,70 @@ struct clk_bulk_data imgsys_isp7_clks[] = {
 		.id = "IMGSYS_CG_IMG_AVS",
 	},
 	{
+		.id = "IMGSYS_CG_IMG_IPS",
+	},
+	{
+		.id = "IMGSYS_CG_SUB_COMMON0",
+	},
+	{
+		.id = "IMGSYS_CG_SUB_COMMON1",
+	},
+	{
+		.id = "IMGSYS_CG_SUB_COMMON2",
+	},
+	{
+		.id = "IMGSYS_CG_SUB_COMMON3",
+	},
+	{
+		.id = "IMGSYS_CG_SUB_COMMON4",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_RX_DIP0",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_RX_DIP1",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_RX_TRAW0",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_RX_WPE0",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_RX_WPE1",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_RX_WPE2",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_RX_IPE0",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_TX_IPE0",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_RX_IPE1",
+	},
+	{
+		.id = "IMGSYS_CG_GALS_TX_IPE1",
+	},
+	{
 		.id = "IMGSYS_CG_IMG_GALS",
 	},
 	{
 		.id = "DIP_TOP_DIP_TOP",
+	},
+	{
+		.id = "DIP_TOP_DIP_TOP_GALS0",
+	},
+	{
+		.id = "DIP_TOP_DIP_TOP_GALS1",
+	},
+	{
+		.id = "DIP_TOP_DIP_TOP_GALS2",
+	},
+	{
+		.id = "DIP_TOP_DIP_TOP_GALS3",
 	},
 	{
 		.id = "DIP_NR1_DIP1_LARB",
@@ -58,13 +115,25 @@ struct clk_bulk_data imgsys_isp7_clks[] = {
 		.id = "WPE1_CG_DIP1_WPE",
 	},
 	{
+		.id = "WPE1_CG_DIP1_GALS0",
+	},
+	{
 		.id = "WPE2_CG_DIP1_WPE",
+	},
+	{
+		.id = "WPE2_CG_DIP1_GALS0",
 	},
 	{
 		.id = "WPE3_CG_DIP1_WPE",
 	},
 	{
+		.id = "WPE3_CG_DIP1_GALS0",
+	},
+	{
 		.id = "TRAW_CG_DIP1_TRAW",
+	},
+	{
+		.id = "TRAW_CG_DIP1_GALS",
 	},
 	{
 		.id = "IMGSYS_CG_IMG_IPE"
