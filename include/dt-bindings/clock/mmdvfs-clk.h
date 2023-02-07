dfc162c55c82bdc5873720d828ab4b676bebbf00
Ming-Fan Chen <ming-fan.chen@mediatek.com>
2023-02-07 09:42:03 +0800

    [ALPS07694708] mmdvfs: add MMDVFS_MUX and MMDVFS_USER for hwccf dvfs
    add MMDVFS_MUX and MMDVFS_USER for hwccf dvfs
    
    MTK-Commit-Id: 899cb16a48d9742a7984a290b6e2de724e02f14d
    
    CR-Id: ALPS07694708
    Feature: mmdvfs
    Signed-off-by: Ming-Fan Chen <ming-fan.chen@mediatek.com>
    Change-Id: Ic1d3f6f5a1407a2047a9d34935e2ce00da0a6edb

--- include/dt-bindings/clock/mmdvfs-clk.h
+++ include/dt-bindings/clock/mmdvfs-clk.h
@@ -7,46 +7,71 @@
 #ifndef _DT_BINDINGS_MMDVFS_CLK_H
 #define _DT_BINDINGS_MMDVFS_CLK_H
 
+/* v2 */
+#define MMDVFS_MUX_DIS		0
+#define MMDVFS_MUX_MDP		1
+#define MMDVFS_MUX_MML		2
+#define MMDVFS_MUX_SMI		3
+#define MMDVFS_MUX_VEN		4
+#define MMDVFS_MUX_VDE		5
+#define MMDVFS_MUX_IMG		6
+#define MMDVFS_MUX_CAM		7
+#define MMDVFS_MUX_NUM		8
+
+#define MMDVFS_USER_DISP	0
+#define MMDVFS_USER_MDP		1
+#define MMDVFS_USER_MML		2
+#define MMDVFS_USER_SMI		3
+
+#define MMDVFS_USER_JPEGDEC	4
+#define MMDVFS_USER_JPEGENC	5
+#define MMDVFS_USER_VENC	6
+
+#define MMDVFS_USER_VFMT	7
+#define MMDVFS_USER_IMG		8
+#define MMDVFS_USER_CAM		9
+#define MMDVFS_USER_NUM		10
+
 /* clock consumer */
-#define CLK_MMDVFS_DISP		(0)
-#define CLK_MMDVFS_MDP		(1)
-#define CLK_MMDVFS_MML		(2)
-#define CLK_MMDVFS_SMI_COMMON0	(3)
-#define CLK_MMDVFS_SMI_COMMON1	(4)
+#define CLK_MMDVFS_DISP		0
+#define CLK_MMDVFS_MDP		1
+#define CLK_MMDVFS_MML		2
+#define CLK_MMDVFS_SMI_COMMON0	3
+#define CLK_MMDVFS_SMI_COMMON1	4
 
-#define CLK_MMDVFS_VENC		(5)
-#define CLK_MMDVFS_JPEGENC	(6)
+#define CLK_MMDVFS_VENC		5
+#define CLK_MMDVFS_JPEGENC	6
 
-#define CLK_MMDVFS_VDEC		(7)
-#define CLK_MMDVFS_VFMT		(8)
-#define CLK_MMDVFS_JPEGDEC	(9)
+#define CLK_MMDVFS_VDEC		7
+#define CLK_MMDVFS_VFMT		8
+#define CLK_MMDVFS_JPEGDEC	9
 
-#define CLK_MMDVFS_IMG		(10)
-#define CLK_MMDVFS_IPE		(11)
-#define CLK_MMDVFS_CAM		(12)
-#define CLK_MMDVFS_CCU		(13)
-#define CLK_MMDVFS_AOV		(14)
+#define CLK_MMDVFS_IMG		10
+#define CLK_MMDVFS_IPE		11
+#define CLK_MMDVFS_CAM		12
+#define CLK_MMDVFS_CCU		13
+#define CLK_MMDVFS_AOV		14
 
-#define CLK_MMDVFS_VCORE	(15)
-#define CLK_MMDVFS_VMM		(16)
-#define CLK_MMDVFS_VDISP	(17)
+#define CLK_MMDVFS_VCORE	15
+#define CLK_MMDVFS_VMM		16
+#define CLK_MMDVFS_VDISP	17
 
 /* new clk append here */
-#define CLK_MMDVFS_NUM		(18)
+#define CLK_MMDVFS_NUM		18
 
 /* power supplier */
-#define PWR_MMDVFS_VCORE	(0)
-#define PWR_MMDVFS_VMM		(1)
-#define PWR_MMDVFS_VDISP	(2)
-#define PWR_MMDVFS_NUM		(3)
+#define PWR_MMDVFS_VCORE	0
+#define PWR_MMDVFS_VMM		1
+#define PWR_MMDVFS_VDISP	2
+#define PWR_MMDVFS_NUM		3
 
 /* ipi type */
-#define IPI_MMDVFS_VCP		(0)
-#define IPI_MMDVFS_CCU		(1)
+#define IPI_MMDVFS_VCP		0
+#define IPI_MMDVFS_CCU		1
 
 /* spec type */
-#define SPEC_MMDVFS_NORMAL	(0)
-#define SPEC_MMDVFS_ALONE	(1)
-#define SPEC_MMDVFS_DVFSRC	(2)
+#define SPEC_MMDVFS_NORMAL	0
+#define SPEC_MMDVFS_ALONE	1
+#define SPEC_MMDVFS_DVFSRC	2
 
 #endif /* _DT_BINDINGS_MMDVFS_CLK_H */
