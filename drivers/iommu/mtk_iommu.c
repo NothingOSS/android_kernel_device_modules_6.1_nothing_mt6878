6691bad2a52df0bda519f84996b489b8f5d0cfb1
Qiming Sun <qiming.sun@mediatek.com>
2023-02-23 09:43:16 +0800

    [ALPS07676318] iommu/mediatek: mt6897: add iommu flag PGTABLE_PA_35_EN
    Add PGTABLE_PA_35_EN to allow the PA of pgtables support up to bit35.
    
    MTK-Commit-Id: 32f10fa48a7cfd97b4b580e9c00a35d33f0c7335
    
    Signed-off-by: Qiming Sun <qiming.sun@mediatek.com>
    CR-Id: ALPS07676318
    Feature: iommu_kernel
    Change-Id: Ibeba8c5f9c9293092e17daa988fdcc7bc0fc635b

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -3539,8 +3539,8 @@ static const struct mtk_iommu_plat_data mt6897_data_disp = {
 	.m4u_plat	= M4U_MT6897,
 	.flags          = OUT_ORDER_WR_EN | GET_DOM_ID_LEGACY |
 			  NOT_STD_AXI_MODE | TLB_SYNC_EN | IOMMU_SEC_EN |
-			  SKIP_CFG_PORT | IOVA_34_EN | HAS_BCLK |
-			  HAS_SMI_SUB_COMM | SAME_SUBSYS | IOMMU_MAU_EN,
+			  SKIP_CFG_PORT | IOVA_34_EN | PGTABLE_PA_35_EN |
+			  HAS_BCLK | HAS_SMI_SUB_COMM | SAME_SUBSYS | IOMMU_MAU_EN,
 	.hw_list        = &mm_iommu_list,
 	.inv_sel_reg    = REG_MMU_INV_SEL_GEN2,
 	.iommu_id	= DISP_IOMMU,
@@ -3556,8 +3556,8 @@ static const struct mtk_iommu_plat_data mt6897_data_mdp = {
 	.m4u_plat	= M4U_MT6897,
 	.flags          = OUT_ORDER_WR_EN | GET_DOM_ID_LEGACY |
 			  NOT_STD_AXI_MODE | TLB_SYNC_EN | IOMMU_SEC_EN |
-			  SKIP_CFG_PORT | IOVA_34_EN | HAS_BCLK |
-			  HAS_SMI_SUB_COMM | SAME_SUBSYS | IOMMU_MAU_EN,
+			  SKIP_CFG_PORT | IOVA_34_EN | PGTABLE_PA_35_EN |
+			  HAS_BCLK | HAS_SMI_SUB_COMM | SAME_SUBSYS | IOMMU_MAU_EN,
 	.hw_list        = &mm_iommu_list,
 	.inv_sel_reg    = REG_MMU_INV_SEL_GEN2,
 	.iommu_id	= MDP_IOMMU,
@@ -3571,7 +3571,7 @@ static const struct mtk_iommu_plat_data mt6897_data_mdp = {
 
 static const struct mtk_iommu_plat_data mt6897_data_apu0 = {
 	.m4u_plat	= M4U_MT6897,
-	.flags          = TLB_SYNC_EN | IOMMU_SEC_EN |
+	.flags          = TLB_SYNC_EN | IOMMU_SEC_EN | PGTABLE_PA_35_EN |
 			  GET_DOM_ID_LEGACY | IOVA_34_EN | LINK_WITH_APU | IOMMU_MAU_EN |
 			  PM_OPS_SKIP,
 	.hw_list        = &apu_iommu_list,
@@ -3587,7 +3587,7 @@ static const struct mtk_iommu_plat_data mt6897_data_apu0 = {
 
 static const struct mtk_iommu_plat_data mt6897_data_apu1 = {
 	.m4u_plat	= M4U_MT6897,
-	.flags          = TLB_SYNC_EN | IOMMU_SEC_EN |
+	.flags          = TLB_SYNC_EN | IOMMU_SEC_EN | PGTABLE_PA_35_EN |
 			  GET_DOM_ID_LEGACY | IOVA_34_EN | LINK_WITH_APU | IOMMU_MAU_EN |
 			  PM_OPS_SKIP,
 	.hw_list        = &apu_iommu_list,
