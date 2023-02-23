fb02385588b501d1f24f2ce8f4d0110560c88b29
Irene.Luo <irene.luo@mediatek.com>
2023-02-23 09:34:50 +0800

    [ALPS07789992] PMSR: MT6897: Add PMSR v2 user interface
    1. Add v2 user interface for supporting mt6897
    2. Keep the original interface for supporting legacy project
    3. Add a dts node handler for parsing the dpmsr count
    
    MTK-Commit-Id: a70beaf3c3bd7717989a73a738e2806321bb6ce3
    
    Signed-off-by: Irene.Luo <irene.luo@mediatek.com>
    CR-Id: ALPS07789992
    Feature: SWPM SSPM
    Change-Id: I1d0589830bf23a5b796718f3fa4672860b08e9ac

---
+++ drivers/misc/mediatek/pmsr/v2/pmsr.h
@@ -0,0 +1,79 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2019 MediaTek Inc.
+ */
+//#include <linux/proc_fs.h>
+//#include <linux/uaccess.h>
+
+#ifndef __PMSR_H__
+#define __PMSR_H__
+
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+
+#define MONTYPE_RISING			(0)
+#define MONTYPE_FALLING			(1)
+#define MONTYPE_HIGH_LEVEL		(2)
+#define MONITPE_LOW_LEVEL		(3)
+
+/* Must equal to CODA */
+#define SPEED_MODE_NORMAL		(0x2)
+#define SPEED_MODE_SPEED		(0x3)
+
+#define DEFAULT_MONTYPE			MONTYPE_HIGH_LEVEL
+#define DEFAULT_SPEED_MODE		SPEED_MODE_NORMAL
+
+#define PMSR_PERIOD_MS			(1000)
+#define WINDOW_LEN_SPEED		(PMSR_PERIOD_MS * 0x65B8)
+#define WINDOW_LEN_NORMAL		(PMSR_PERIOD_MS * 0xD)
+#define GET_EVENT_RATIO_SPEED(x)	((x)/(WINDOW_LEN_SPEED/1000))
+#define GET_EVENT_RATIO_NORMAL(x)	((x)/(WINDOW_LEN_NORMAL/1000))
+
+#define PMSR_TOOL_ACT_ENABLE		(1u << 0)
+#define PMSR_TOOL_ACT_DISABLE		(1u << 1)
+#define PMSR_TOOL_ACT_WINDOW		(1u << 2)
+#define PMSR_TOOL_ACT_SIGNAL		(1u << 3)
+#define PMSR_TOOL_ACT_MONTYPE		(1u << 4)
+#define PMSR_TOOL_ACT_SIGNUM		(1u << 5)
+#define PMSR_TOOL_ACT_EN			(1u << 6)
+
+#define SET_CH_MAX 4
+#define MTK_PMSR_BUF_WRITESZ 512
+
+#if IS_ENABLED(CONFIG_MTK_TINYSYS_SCMI)
+struct pmsr_tool_scmi_data {
+	unsigned int uid	: 24;
+	unsigned int magic	: 8;
+	unsigned int action;
+	unsigned int param1;
+	unsigned int param2;
+	unsigned int param3;
+};
+#endif
+
+struct pmsr_channel {
+	unsigned int dpmsr_id;
+	unsigned int signal_id;
+};
+
+struct pmsr_dpmsr_cfg {
+	unsigned int montype;	/* 2 bits, monitor type */
+	unsigned int signum;
+	unsigned int en;
+};
+
+struct pmsr_cfg {
+	struct pmsr_dpmsr_cfg *dpmsr;
+	struct pmsr_channel ch[SET_CH_MAX];	/* channel 0~3 config */
+	unsigned int pmsr_speed_mode;		/* 0: normal, 1: high speed */
+	unsigned int pmsr_window_len;
+	bool enable;
+	unsigned int dpmsr_count;
+	/* pmsr_window_len
+	 *	0: will be automatically updated for current speed mode
+	 * for normal speed mode, set to WINDOW_LEN_NORMAL.
+	 * for high speed mode, set to WINDOW_LEN_SPEED.
+	 */
+};
+#endif /* __PMSR_H__ */
+
