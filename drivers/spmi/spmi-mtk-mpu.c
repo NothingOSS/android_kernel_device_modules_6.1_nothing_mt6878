26caec2da2eb91832a5ecbb4e86dc76eba399ec7
Roy-fp Wu <roy-fp.wu@mediatek.com>
2023-03-09 11:58:20 +0800

    [ALPS07818141] PMIF: modify MPU compatible naming for mt6897
    Modidy MPU compatible naming for mt6897
    
    MTK-Commit-Id: eda828ac4b84d042538fb33742d5ea849f0b6f83
    
    Signed-off-by: Roy-fp Wu <roy-fp.wu@mediatek.com>
    CR-Id: ALPS07818141
    Feature: pmif_driver
    Change-Id: I8bdb17a6de9d40220fda8ea0d0ab32ad49159c74

--- drivers/spmi/spmi-mtk-mpu.c
+++ drivers/spmi/spmi-mtk-mpu.c
@@ -172,7 +172,7 @@ static const struct of_device_id mtk_spmi_pmif_mpu_match_table[] = {
 		.compatible = "mediatek,mt6895-spmi_pmif_mpu",
 		.data = &pmif_mpu_arb,
 	}, {
-		.compatible = "mediatek,mt6897-spmi_pmif_mpu",
+		.compatible = "mediatek,mt6897-spmi-pmif-mpu",
 		.data = &pmif_mpu_arb,
 	}, {
 		.compatible = "mediatek,mt6983-spmi_pmif_mpu",
