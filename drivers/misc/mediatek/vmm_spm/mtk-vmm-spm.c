9d9cac608d0355752a76217b20ce26a1cf394f09
Nick wen <nick.wen@mediatek.com>
2023-04-15 14:02:50 +0800

    [ALPS07824959] add iso check & refactor flow
    1. add iso check flow
    2. refactor iso notify flow
    
    MTK-Commit-Id: 0137410cf726af00bc20fd40a7dd7047cced16cb
    
    Signed-off-by: Nick wen <nick.wen@mediatek.com>
    CR-Id: ALPS07824959
    Feature: mmdvfs
    Change-Id: I060b0e8d51b9a8b5f7608c548941f0be8a32405e

--- drivers/misc/mediatek/vmm_spm/mtk-vmm-spm.c
+++ drivers/misc/mediatek/vmm_spm/mtk-vmm-spm.c
@@ -33,6 +33,8 @@
 	pr_notice("[ISPDVFS] fatal %s(),%d: " fmt "\n", \
 		__func__, __LINE__, ##args)
 
+#define SPM_CHECK_IDX(idx, max) ((idx) >= (max) ? 1 : 0)
+
 #define SPM_VMM_HW_SEM_REG_OFST 0x6A8
 #define SPM_VMM_ISO_REG_OFST 0xF30
 #define SPM_VMM_ISO_REG_OFST_MT6886 0xF74
@@ -58,12 +60,25 @@
 #define SOC_BUCK_ISO_CON_SET 0xF7C
 #define SOC_BUCK_ISO_CON_CLR 0xF80
 
-/* Buck ISO control for 7sp */
-#define SPM_VMM_EXT_BUCK_ISO_BIT_7SP 20
-#define SPM_AOC_VMM_SRAM_ISO_DIN_BIT_7SP 21
-#define SPM_AOC_VMM_SRAM_LATCH_ENB_7SP 22
-#define SOC_BUCK_ISO_CON_SET_7SP 0xFA8
-#define SOC_BUCK_ISO_CON_CLR_7SP 0xFAC
+static unsigned int spm_ver;
+
+enum {
+	VMM_SPM_DEFAULT = 0,
+	VMM_SPM_MT6886,
+	VMM_SPM_7S,
+	VMM_SPM_7SP,
+
+	VMM_SPM_MAX,
+};
+
+struct vmm_spm_reg_info {
+	unsigned int iso_con_status;
+	unsigned int iso_con_set;
+	unsigned int iso_con_clr;
+	unsigned int ext_iso_bit;
+	unsigned int iso_din_bit;
+	unsigned int latch_en_bit;
+};
 
 struct vmm_spm_drv_data {
 	void __iomem *spm_reg;
@@ -71,6 +86,25 @@ struct vmm_spm_drv_data {
 };
 
 static struct vmm_spm_drv_data g_drv_data;
+
+static const struct vmm_spm_reg_info spm_reg_tbl[VMM_SPM_MAX] = {
+	[VMM_SPM_7S] = {
+		.iso_con_status = 0xF78,
+		.iso_con_set = 0xF7C,
+		.iso_con_clr = 0xF80,
+		.ext_iso_bit = 20,
+		.iso_din_bit = 21,
+		.latch_en_bit = 22,
+	},
+	[VMM_SPM_7SP] = {
+		.iso_con_status = 0xFA4,
+		.iso_con_set = 0xFA8,
+		.iso_con_clr = 0xFAC,
+		.ext_iso_bit = 20,
+		.iso_din_bit = 21,
+		.latch_en_bit = 22,
+	},
+};
 
 static void vmm_dump_spm_sem_reg(void __iomem *base)
 {
@@ -203,27 +237,6 @@ static void vmm_buck_isolation_on(void __iomem *base)
 	release_hw_semaphore(hw_sem_addr, &flags);
 }
 
-static int regulator_event_notify(struct notifier_block *nb,
-				  unsigned long event, void *data)
-{
-	struct vmm_spm_drv_data *drv_data = &g_drv_data;
-
-	if (!drv_data->spm_reg) {
-		ISP_LOGE("SPM_BASE is NULL");
-		return -EINVAL;
-	}
-
-	if (event == REGULATOR_EVENT_ENABLE) {
-		vmm_buck_isolation_off(drv_data->spm_reg);
-		ISP_LOGI("VMM regulator enable done");
-	} else if (event == REGULATOR_EVENT_PRE_DISABLE) {
-		vmm_buck_isolation_on(drv_data->spm_reg);
-		ISP_LOGI("VMM regulator before disable");
-	}
-
-	return 0;
-}
-
 static void vmm_buck_isolation_off_mt6886(void __iomem *base)
 {
 	void __iomem *reg_buck_iso_addr = base + SPM_VMM_ISO_REG_OFST_MT6886;
@@ -280,7 +293,74 @@ static void vmm_buck_isolation_on_mt6886(void __iomem *base)
 	release_hw_semaphore(hw_sem_addr, &flags);
 }
 
-static int regulator_event_notify_mt6886(struct notifier_block *nb,
+static void vmm_buck_isolation_off_general(void __iomem *base)
+{
+	void __iomem *addr;
+
+	if (SPM_CHECK_IDX(spm_ver, VMM_SPM_MAX)) {
+		ISP_LOGE("[fatal]VMM spm version[cur=%u/max=%u] error!\n",
+				spm_ver, VMM_SPM_MAX);
+		return;
+	}
+
+	addr = base + spm_reg_tbl[spm_ver].iso_con_clr;
+	writel_relaxed((1 << spm_reg_tbl[spm_ver].ext_iso_bit), addr);
+	writel_relaxed((1 << spm_reg_tbl[spm_ver].iso_din_bit), addr);
+	writel_relaxed((1 << spm_reg_tbl[spm_ver].latch_en_bit), addr);
+}
+
+static void vmm_buck_isolation_on_general(void __iomem *base)
+{
+	void __iomem *addr;
+
+	if (SPM_CHECK_IDX(spm_ver, VMM_SPM_MAX)) {
+		ISP_LOGE("[fatal]VMM spm version[cur=%u/max=%u] error!\n",
+				spm_ver, VMM_SPM_MAX);
+		return;
+	}
+
+	addr = base + spm_reg_tbl[spm_ver].iso_con_set;
+	writel_relaxed((1 << spm_reg_tbl[spm_ver].latch_en_bit), addr);
+	writel_relaxed((1 << spm_reg_tbl[spm_ver].iso_din_bit), addr);
+	writel_relaxed((1 << spm_reg_tbl[spm_ver].ext_iso_bit), addr);
+}
+
+static void vmm_buck_isolation_check_reg(void __iomem *base, bool iso_on)
+{
+	void __iomem *addr;
+	u32 iso_val;
+
+	if (SPM_CHECK_IDX(spm_ver, VMM_SPM_MAX)) {
+		ISP_LOGE("[fatal]VMM spm version[cur=%u/max=%u] error!\n",
+				spm_ver, VMM_SPM_MAX);
+		return;
+	}
+
+	addr = base + spm_reg_tbl[spm_ver].iso_con_status;
+	iso_val = readl_relaxed(addr);
+	if (iso_on) {
+		if (((iso_val & BIT(spm_reg_tbl[spm_ver].latch_en_bit))
+			!= BIT(spm_reg_tbl[spm_ver].latch_en_bit)) ||
+			((iso_val & BIT(spm_reg_tbl[spm_ver].iso_din_bit))
+			!= BIT(spm_reg_tbl[spm_ver].iso_din_bit)) ||
+			((iso_val & BIT(spm_reg_tbl[spm_ver].ext_iso_bit))
+			!= BIT(spm_reg_tbl[spm_ver].ext_iso_bit))) {
+			ISP_LOGE("ISO on error! iso_val[0x%x] = %u, spm_ver=%d\n",
+				spm_reg_tbl[spm_ver].iso_con_status, iso_val, spm_ver);
+			panic("VMM ISO set on wrong!");
+		}
+	} else {
+		if (((iso_val & BIT(spm_reg_tbl[spm_ver].latch_en_bit)) != 0) ||
+			((iso_val & BIT(spm_reg_tbl[spm_ver].iso_din_bit)) != 0) ||
+			((iso_val & BIT(spm_reg_tbl[spm_ver].ext_iso_bit)) != 0)) {
+			ISP_LOGE("ISO off_error!!!!!!!!!! iso_val[0x%x] = %u, spm_ver=%d\n",
+				spm_reg_tbl[spm_ver].iso_con_status, iso_val, spm_ver);
+			panic("VMM ISO set off wrong!");
+		}
+	}
+}
+
+static int regulator_event_notify(struct notifier_block *nb,
 				  unsigned long event, void *data)
 {
 	struct vmm_spm_drv_data *drv_data = &g_drv_data;
@@ -291,89 +371,25 @@ static int regulator_event_notify_mt6886(struct notifier_block *nb,
 	}
 
 	if (event == REGULATOR_EVENT_ENABLE) {
-		vmm_buck_isolation_off_mt6886(drv_data->spm_reg);
-		ISP_LOGI("VMM regulator enable done");
+		if (spm_ver == VMM_SPM_MT6886)
+			vmm_buck_isolation_off_mt6886(drv_data->spm_reg);
+		else if (spm_ver == VMM_SPM_DEFAULT)
+			vmm_buck_isolation_off(drv_data->spm_reg);
+		else {
+			vmm_buck_isolation_off_general(drv_data->spm_reg);
+			vmm_buck_isolation_check_reg(drv_data->spm_reg, false);
+		}
+		ISP_LOGI("VMM regulator enable done, ver = %u\n", spm_ver);
 	} else if (event == REGULATOR_EVENT_PRE_DISABLE) {
-		vmm_buck_isolation_on_mt6886(drv_data->spm_reg);
-		ISP_LOGI("VMM regulator before disable");
-	}
-
-	return 0;
-}
-
-static void vmm_buck_isolation_off_7s(void __iomem *base)
-{
-	void __iomem *addr = base + SOC_BUCK_ISO_CON_CLR;
-
-	writel_relaxed((1 << SPM_VMM_EXT_BUCK_ISO_BIT_7S), addr);
-	writel_relaxed((1 << SPM_AOC_VMM_SRAM_ISO_DIN_BIT_7S), addr);
-	writel_relaxed((1 << SPM_AOC_VMM_SRAM_LATCH_ENB_7S), addr);
-}
-
-static void vmm_buck_isolation_on_7s(void __iomem *base)
-{
-	void __iomem *addr = base + SOC_BUCK_ISO_CON_SET;
-
-	writel_relaxed((1 << SPM_AOC_VMM_SRAM_LATCH_ENB_7S), addr);
-	writel_relaxed((1 << SPM_AOC_VMM_SRAM_ISO_DIN_BIT_7S), addr);
-	writel_relaxed((1 << SPM_VMM_EXT_BUCK_ISO_BIT_7S), addr);
-}
-
-static int regulator_event_notify_7s(struct notifier_block *nb,
-				  unsigned long event, void *data)
-{
-	struct vmm_spm_drv_data *drv_data = &g_drv_data;
-
-	if (!drv_data->spm_reg) {
-		ISP_LOGE("SPM_BASE is NULL");
-		return -EINVAL;
-	}
-
-	if (event == REGULATOR_EVENT_ENABLE) {
-		vmm_buck_isolation_off_7s(drv_data->spm_reg);
-		ISP_LOGI("VMM 7s regulator enable done");
-	} else if (event == REGULATOR_EVENT_PRE_DISABLE) {
-		vmm_buck_isolation_on_7s(drv_data->spm_reg);
-		ISP_LOGI("VMM 7s regulator before disable");
-	}
-
-	return 0;
-}
-
-static void vmm_buck_isolation_off_7sp(void __iomem *base)
-{
-	void __iomem *addr = base + SOC_BUCK_ISO_CON_CLR_7SP;
-
-	writel_relaxed((1 << SPM_VMM_EXT_BUCK_ISO_BIT_7SP), addr);
-	writel_relaxed((1 << SPM_AOC_VMM_SRAM_ISO_DIN_BIT_7SP), addr);
-	writel_relaxed((1 << SPM_AOC_VMM_SRAM_LATCH_ENB_7SP), addr);
-}
-
-static void vmm_buck_isolation_on_7sp(void __iomem *base)
-{
-	void __iomem *addr = base + SOC_BUCK_ISO_CON_SET_7SP;
-
-	writel_relaxed((1 << SPM_AOC_VMM_SRAM_LATCH_ENB_7SP), addr);
-	writel_relaxed((1 << SPM_AOC_VMM_SRAM_ISO_DIN_BIT_7SP), addr);
-	writel_relaxed((1 << SPM_VMM_EXT_BUCK_ISO_BIT_7SP), addr);
-}
-
-static int regulator_event_notify_7sp(struct notifier_block *nb,
-				  unsigned long event, void *data)
-{
-	struct vmm_spm_drv_data *drv_data = &g_drv_data;
-
-	if (!drv_data->spm_reg) {
-		ISP_LOGE("SPM_BASE is NULL");
-		return -EINVAL;
-	}
-
-	if (event == REGULATOR_EVENT_ENABLE) {
-		vmm_buck_isolation_off_7sp(drv_data->spm_reg);
-		ISP_LOGI("VMM 7sp regulator enable done");
-	} else if (event == REGULATOR_EVENT_PRE_DISABLE) {
-		vmm_buck_isolation_on_7sp(drv_data->spm_reg);
-		ISP_LOGI("VMM 7sp regulator before disable");
+		if (spm_ver == VMM_SPM_MT6886)
+			vmm_buck_isolation_on_mt6886(drv_data->spm_reg);
+		else if (spm_ver == VMM_SPM_DEFAULT)
+			vmm_buck_isolation_on(drv_data->spm_reg);
+		else {
+			vmm_buck_isolation_on_general(drv_data->spm_reg);
+			vmm_buck_isolation_check_reg(drv_data->spm_reg, true);
+		}
+		ISP_LOGI("VMM regulator before disable, ver = %u\n", spm_ver);
 	}
 
 	return 0;
@@ -407,13 +423,15 @@ static int vmm_spm_probe(struct platform_device *pdev)
 	}
 
 	if (of_device_is_compatible(dev->of_node, "mediatek,vmm_spm_7sp"))
-		drv_data->nb.notifier_call = regulator_event_notify_7sp;
+		spm_ver = VMM_SPM_7SP;
 	else if (of_device_is_compatible(dev->of_node, "mediatek,vmm_spm_7s"))
-		drv_data->nb.notifier_call = regulator_event_notify_7s;
+		spm_ver = VMM_SPM_7S;
 	else if (of_device_is_compatible(dev->of_node, "mediatek,vmm_spm_mt6886"))
-		drv_data->nb.notifier_call = regulator_event_notify_mt6886;
+		spm_ver = VMM_SPM_MT6886;
 	else
-		drv_data->nb.notifier_call = regulator_event_notify;
+		spm_ver = VMM_SPM_DEFAULT;
+
+	drv_data->nb.notifier_call = regulator_event_notify;
 	ret = devm_regulator_register_notifier(reg, &drv_data->nb);
 	if (ret)
 		ISP_LOGE("Failed to register notifier: %d\n", ret);
@@ -436,6 +454,45 @@ static const struct of_device_id of_vmm_spm_match_tbl[] = {
 	},
 	{}
 };
+
+int mtk_vmm_spm_get_data(const char *val, const struct kernel_param *kp)
+{
+	struct vmm_spm_drv_data *drv_data = &g_drv_data;
+	void __iomem *addr;
+	void __iomem *base;
+
+	if (!drv_data->spm_reg || spm_ver >= VMM_SPM_MAX) {
+		ISP_LOGE("SPM_BASE is NULL or spm_ver[%u] is wrong. Max=%u", spm_ver, VMM_SPM_MAX);
+		return -EINVAL;
+	}
+
+	base = drv_data->spm_reg;
+
+	addr = base + spm_reg_tbl[spm_ver].iso_con_status;
+	ISP_LOGI("%s iso_con_status[0x%x] = 0x%x\n",
+			__func__,
+			spm_reg_tbl[spm_ver].iso_con_status,
+			readl_relaxed(addr));
+	addr = base + spm_reg_tbl[spm_ver].iso_con_set;
+	ISP_LOGI("%s iso_con_set[0x%x] = 0x%x\n",
+			__func__,
+			spm_reg_tbl[spm_ver].iso_con_set,
+			readl_relaxed(addr));
+	addr = base + spm_reg_tbl[spm_ver].iso_con_clr;
+	ISP_LOGI("%s iso_con_clr[0x%x] = 0x%x\n",
+			__func__,
+			spm_reg_tbl[spm_ver].iso_con_clr,
+			readl_relaxed(addr));
+	return 0;
+}
+
+static const struct kernel_param_ops vmm_spm_get_data = {
+	.set = mtk_vmm_spm_get_data,
+};
+
+module_param_cb(vmm_spm_info, &vmm_spm_get_data, NULL, 0644);
+MODULE_PARM_DESC(vmm_spm_info,
+	"get vmm spm info");
 
 static struct platform_driver drv_vmm_spm = {
 	.probe = vmm_spm_probe,
