878ac285f9960c9b62756a5784d35fc02470967d
Po-Wen Kao <powen.kao@mediatek.com>
2023-04-15 14:05:57 +0800

    [ALPS07820708] ufs: mediatek: Set UFSHCD_QUIRK_MCQ_BROKEN_INTR quirk
    Set UFSHCD_QUIRK_MCQ_BROKEN_INTR for mtk driver
    
    MTK-Commit-Id: db1de0f814a8e6e6632db4f9e8cf6441ccea5e3b
    
    CR-Id: ALPS07820708
    Feature: ufs
    Change-Id: I6ffbd872976e76e5386d3cf8d0447236631b8bce
    Signed-off-by: Po-Wen Kao <powen.kao@mediatek.com>

--- drivers/scsi/ufs/ufs-mediatek.c
+++ drivers/scsi/ufs/ufs-mediatek.c
@@ -2385,6 +2385,8 @@ static int ufs_mtk_init(struct ufs_hba *hba)
 	atomic_set(&host->clkscale_control_powerhal, 0);
 
 	hba->quirks |= UFSHCI_QUIRK_SKIP_MANUAL_WB_FLUSH_CTRL;
+	hba->quirks |= UFSHCD_QUIRK_MCQ_BROKEN_INTR;
+
 	hba->vps->wb_flush_threshold = UFS_WB_BUF_REMAIN_PERCENT(80);
 
 	if (host->caps & UFS_MTK_CAP_DISABLE_AH8)
