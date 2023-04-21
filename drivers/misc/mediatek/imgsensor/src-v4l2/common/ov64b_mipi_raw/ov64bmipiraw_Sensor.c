2688ea45c934b49c7ebc6ef4c911c7959c35ba41
Tony Shen <tung.shen@mediatek.com>
2023-04-21 09:33:28 +0800

    [ALPS07851750] mt6989: Seninf Porting
    [Description]
    Code Ready before SMT.
    [Detail]
    1. Modify sensor power sequence.
    
    MTK-Commit-Id: e02b63fe7438e878c48e37eaf972d4e7eaecaebe
    
    CR-Id: ALPS07851750
    Feature: [Android Default] Camera Application Basic Functions
    Signed-off-by: Tony Shen <tung.shen@mediatek.com>
    Change-Id: Idc2a3bdfba198de07874edbf94d005029cb43aec

--- drivers/misc/mediatek/imgsensor/src-v4l2/common/ov64b_mipi_raw/ov64bmipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src-v4l2/common/ov64b_mipi_raw/ov64bmipiraw_Sensor.c
@@ -972,11 +972,12 @@ static struct subdrv_ops ops = {
 static struct subdrv_pw_seq_entry pw_seq[] = {
 	{HW_ID_MCLK, 24, 0},
 	{HW_ID_RST, 0, 1},
-	{HW_ID_AVDD, 2800000, 3},
-	{HW_ID_AFVDD, 3100000, 3},
-	{HW_ID_DVDD, 1100000, 4},
-	{HW_ID_DOVDD, 1800000, 1},
-	{HW_ID_MCLK_DRIVING_CURRENT, 8, 6},
+	{HW_ID_AVDD, 2800000, 1}, // pmic_ldo for avdd
+	{HW_ID_AFVDD, 3100000, 1}, // pmic_ldo for afvdd
+	{HW_ID_AFVDD1, 1800000, 1}, // pmic_gpo(3.1V ldo) for afvdd
+	{HW_ID_DVDD, 1100000, 1}, // pmic_ldo/gpio(1.1V ldo) for dvdd
+	{HW_ID_DOVDD, 1800000, 1}, // pmic_ldo/gpio(1.8V ldo) for dovdd
+	{HW_ID_MCLK_DRIVING_CURRENT, 8, 5},
 	{HW_ID_RST, 1, 5}
 };
 
