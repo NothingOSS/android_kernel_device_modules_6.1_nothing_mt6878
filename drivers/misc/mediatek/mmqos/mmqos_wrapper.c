f46ae0c07d1be514c915db5672210194c1b6bfd9
wendy-st.lin <wendy-st.lin@mediatek.com>
2023-05-02 14:43:29 +0800

    [ALPS07778378] mmqos: mt6989: mmqos porting
    Add BUILD.bazel, mmqos-mt6989.c and mmqos_wrapper
    
    MTK-Commit-Id: b69c8b270e1f92fa3ee57cbb823013f41481c353
    
    Signed-off-by: wendy-st.lin <wendy-st.lin@mediatek.com>
    CR-Id: ALPS07778378
    Feature: MMQoS
    Change-Id: Ib3d2b07bcd61f01a50cc4bf4a0b0a1c9e7483ce0

--- drivers/misc/mediatek/mmqos/mmqos_wrapper.c
+++ drivers/misc/mediatek/mmqos/mmqos_wrapper.c
@@ -43,6 +43,10 @@ static struct wrapper_data wrapper_data_mt6985 = {
 	.max_ostd = 40,
 	.icc_dst_id = SLAVE_COMMON(0),
 };
+static struct wrapper_data wrapper_data_mt6989 = {
+	.max_ostd = 40,
+	.icc_dst_id = SLAVE_COMMON(0),
+};
 static struct wrapper_data *mmqos_wrapper;
 static struct device *dev;
 static BLOCKING_NOTIFIER_HEAD(hrt_bw_throttle_notifier);
@@ -238,6 +242,10 @@ static const struct of_device_id of_mmqos_wrapper_match_tbl[] = {
 	{
 		.compatible = "mediatek,mt6985-mmqos-wrapper",
 		.data = &wrapper_data_mt6985,
+	},
+	{
+		.compatible = "mediatek,mt6989-mmqos-wrapper",
+		.data = &wrapper_data_mt6989,
 	},
 	{}
 };
