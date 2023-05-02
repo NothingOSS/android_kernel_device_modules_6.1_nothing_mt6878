cbd1fc09bb2fd898ccafb18e487fa800576a8898
mtk23564 <baron-pj.yen@mediatek.com>
2023-05-02 14:40:21 +0800

    [ALPS07993152] cam: sensor: remove old kernel repo
    [Detail]
    1. cam: sensor: old kernel module repo removed
    
    MTK-Commit-Id: 340e41003c7acba1a628d9635e150432affa6d65
    
    Signed-off-by: mtk23564 <baron-pj.yen@mediatek.com>
    CR-Id: ALPS07993152
    Feature: camsys
    Change-Id: Ic0781bed1a8b0ae09d07828fed68db04f16e13e3

--- drivers/misc/mediatek/imgsensor/src-v4l2/virt-sensor/virt-sensor.mk
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2020 MediaTek Inc.
-
-subdir-ccflags-y += \
-	-I$(IMGSENSOR_DRIVER_PATH)/$(VIRT_SENSOR) \
-
-imgsensor-objs += $(VIRT_SENSOR)/virt-sensor-drv.o
-
