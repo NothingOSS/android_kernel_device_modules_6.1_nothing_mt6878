ff5b3119d322e1c34a62e188541d41e53b0fb4a4
Eric Chung <eric.chung@mediatek.com>
2023-05-09 16:20:25 +0800

    [ALPS07820538] gpufreq: mt6989: support dlpt batt oc and low batt
    Support DLPT BATT_OC and LOW_BATT.
    
    MTK-Commit-Id: 1c152224b1efe116c3ca509364834c836bffc071
    
    Signed-off-by: Eric Chung <eric.chung@mediatek.com>
    CR-Id: ALPS07820538
    Feature: MFGSYS driver
    Change-Id: Ib02689f068bdad90606aa91fee6a85e79d524c2d

--- drivers/gpu/mediatek/gpufreq/v2/include/gpufreq_mt6989.h
+++ drivers/gpu/mediatek/gpufreq/v2/include/gpufreq_mt6989.h
@@ -194,16 +194,16 @@ struct gpufreq_core_mask_info g_core_mask_table[SHADER_CORE_NUM] = {
 /**************************************************
  * Power Throttling Setting
  **************************************************/
-#define GPUFREQ_BATT_OC_ENABLE              (0)
+#define GPUFREQ_BATT_OC_ENABLE              (1)
 #define GPUFREQ_BATT_PERCENT_ENABLE         (0)
-#define GPUFREQ_LOW_BATT_ENABLE             (0)
-#define GPUFREQ_BATT_OC_FREQ                (483000)
+#define GPUFREQ_LOW_BATT_ENABLE             (1)
+#define GPUFREQ_BATT_OC_FREQ                (546000)
 #define GPUFREQ_BATT_PERCENT_LV1_FREQ       (780000)
 #define GPUFREQ_BATT_PERCENT_LV2_FREQ       (780000)
 #define GPUFREQ_BATT_PERCENT_LV3_FREQ       (681000)
-#define GPUFREQ_LOW_BATT_LV1_FREQ           (981000)
-#define GPUFREQ_LOW_BATT_LV2_FREQ           (483000)
-#define GPUFREQ_LOW_BATT_LV3_FREQ           (265000)
+#define GPUFREQ_LOW_BATT_LV1_FREQ           (962000)
+#define GPUFREQ_LOW_BATT_LV2_FREQ           (546000)
+#define GPUFREQ_LOW_BATT_LV3_FREQ           (260000)
 
 /**************************************************
  * DVFS Constraint Setting
