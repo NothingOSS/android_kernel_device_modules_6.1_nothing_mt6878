0d9c5123df0bd07176aca858821f9e824c4b148d
jayce.hsu <jayce.hsu@mediatek.com>
2023-05-16 09:34:14 +0800

    [ALPS07851426] mminfra: set dma_mask for fake dev
    set dma_mask for fake dev.
    
    MTK-Commit-Id: 78250b3f3e20ea51820beb8c8e41164c8dbc65de
    
    Signed-off-by: jayce.hsu <jayce.hsu@mediatek.com>
    CR-Id: ALPS07851426
    Feature: mminfra
    Change-Id: Iadeaf1cd52b5067d81d2251899b1f42a95f4c6a7

--- drivers/misc/mediatek/mminfra/mm-fake-engine.c
+++ drivers/misc/mediatek/mminfra/mm-fake-engine.c
@@ -9,10 +9,37 @@
 #include <linux/of_address.h>
 #include <linux/of_platform.h>
 #include <linux/platform_device.h>
+#include <linux/dma-mapping.h>
+
+static void mm_config_dma_mask(struct device *dev)
+{
+	u32 dma_mask_bit = 0;
+	s32 ret;
+
+	ret = of_property_read_u32(dev->of_node, "dma-mask-bit",
+		&dma_mask_bit);
+
+	if (ret != 0 || !dma_mask_bit)
+		return;
+
+	ret = dma_set_mask_and_coherent(dev, DMA_BIT_MASK(dma_mask_bit));
+	pr_notice("%s set dma mask bit:%u result:%d\n", __func__,
+		dma_mask_bit, ret);
+
+	if (dev->dma_parms) {
+		ret = dma_set_max_seg_size(dev, UINT_MAX);
+		if (ret)
+			pr_notice("%s Failed to set DMA segment size\n", __func__);
+	}
+}
 
 static int mm_fake_eng_probe(struct platform_device *pdev)
 {
+	struct device *dev = &pdev->dev;
+
 	pr_notice("%s for smmu fake dev", __func__);
+	mm_config_dma_mask(dev);
+
 	return 0;
 }
 
