5374e466bccd338283d49417321a4f8009383806
Clive Lin <clive.lin@mediatek.com>
2023-05-31 15:37:24 +0800

    [ALPS08034057] cpu_dvfs: For API control
    API control for powerhal.
    
    MTK-Commit-Id: 69f4e7f31edf9a76847fc3eaa85773009aae3f85
    
    Signed-off-by: Clive Lin <clive.lin@mediatek.com>
    CR-Id: ALPS08034057
    Feature: CPU DVFS (CPU Dynamic Voltage and Frequency Scaling)
    Change-Id: Id0de8abddb4c194100366658679327c6439fe440

--- drivers/misc/mediatek/cpufreq_lite/cpufreq-dbg-lite.c
+++ drivers/misc/mediatek/cpufreq_lite/cpufreq-dbg-lite.c
@@ -27,15 +27,21 @@
 #include "../mcupm/include/mcupm_driver.h"
 #include "../mcupm/include/mcupm_ipi_id.h"
 #include "cpufreq-dbg-lite.h"
+#include "sugov/cpufreq.h"
 
 #ifdef pr_fmt
 #undef pr_fmt
 #endif
 #define pr_fmt(fmt) "[cpudvfs]: " fmt
 
+static void __iomem *csram_base;
+
 u32 *g_cpufreq_debug;
 
 unsigned int cpufreq_debug_cpu;
+unsigned int user_ctrl_mode;
+bool dsu_ctrl_deubg_enable;
+
 static int cpufreq_debug_proc_show(struct seq_file *m, void *v)
 {
 	struct cpufreq_policy *policy;
@@ -143,19 +149,80 @@ static int create_cpufreq_debug_fs(void)
 	}
 
 	for (i = 0; i < ARRAY_SIZE(entries); i++) {
+		pr_info("%s created!\n", entries[i].name);
 		if (!proc_create_data
 			(entries[i].name, 0664,
 			dir, entries[i].fops, NULL))
 			pr_info("%s(), create /proc/cpudvfs/%s failed\n",
-						__func__, entries[0].name);
+						__func__, entries[i].name);
 	}
 	return 0;
 }
+
+void set_cci_mode(unsigned int mode)
+{
+	/* mode = 0(Normal as 50%) mode = 1(Perf as 70%) */
+	csram_write(OFFS_CCI_TBL_MODE, mode);
+}
+
+unsigned int cpufreq_get_cci_mode(void)
+{
+	unsigned int mode;
+
+	/* Normal mode: 0, Perf mode: 1*/
+	mode = csram_read(OFFS_CCI_TBL_MODE);
+
+	if (mode > 1)
+		return -1;
+
+	return mode;
+}
+
+int cpufreq_set_cci_mode(unsigned int mode)
+{
+	if (mode > 1) {
+		pr_info("%s: invalid input value: %d.\n", __func__, mode);
+		return -EINVAL;
+	}
+
+	user_ctrl_mode = mode;
+	set_cci_mode(user_ctrl_mode);
+	if (dsu_ctrl_deubg_enable) {
+		pr_info("%s: debug mode.\n", __func__);
+		return 0;
+	}
+
+	if (user_ctrl_mode)
+		set_eas_dsu_ctrl(0);
+	else
+		set_eas_dsu_ctrl(1);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(cpufreq_set_cci_mode);
+
+int set_dsu_ctrl_debug(unsigned int eas_ctrl_mode, bool debug_enable)
+{
+	dsu_ctrl_deubg_enable = debug_enable;
+
+	if (dsu_ctrl_deubg_enable)
+		set_eas_dsu_ctrl(eas_ctrl_mode);
+	else {
+		if (user_ctrl_mode)
+			set_eas_dsu_ctrl(0);
+		else
+			set_eas_dsu_ctrl(1);
+	}
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(set_dsu_ctrl_debug);
 
 static int mtk_cpudvfs_init(void)
 {
 	struct device_node *dvfs_node;
 	struct platform_device *pdev;
+	struct resource *csram_res;
 
 	dvfs_node = of_find_node_by_name(NULL, "cpuhvfs");
 	if (dvfs_node == NULL) {
@@ -170,6 +237,16 @@ static int mtk_cpudvfs_init(void)
 	}
 
 	create_cpufreq_debug_fs();
+
+	csram_res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
+	csram_base = ioremap(csram_res->start, resource_size(csram_res));
+	if (csram_base == NULL) {
+		pr_notice("failed to map csram_base @ %s\n", __func__);
+		return -EINVAL;
+	}
+	dsu_ctrl_deubg_enable = false;
+	user_ctrl_mode = 0;
+
 	return 0;
 }
 module_init(mtk_cpudvfs_init);
