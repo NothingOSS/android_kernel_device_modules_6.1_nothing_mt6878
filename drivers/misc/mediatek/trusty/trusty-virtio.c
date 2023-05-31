869679c587e9024852afb246647d11fef834cd12
Jackson Chang <jackson-kt.chang@mediatek.com>
2023-05-31 15:35:47 +0800

    [ALPS07883638] iSE: enable irq log
    Enable iSE irq log
    
    MTK-Commit-Id: bb0c7e8b47a54339ebc1b199c12415914ff4fe12
    
    Signed-off-by: Jackson Chang <jackson-kt.chang@mediatek.com>
    CR-Id: ALPS07883638
    Feature: iSE
    Change-Id: I0e0310bbf2868a4dda35850e26baa48a8aa78992

--- drivers/misc/mediatek/trusty/trusty-virtio.c
+++ drivers/misc/mediatek/trusty/trusty-virtio.c
@@ -651,13 +651,13 @@ static irqreturn_t trusty_virtio_irq_handler(int irq, void *data)
 {
 	struct trusty_ctx *tctx = data;
 
-	dev_dbg(tctx->dev, "enter\n");
+	dev_info(tctx->dev, "%s: enter\n", __func__);
 	trusty_notifier_call();
 
 	if (!(trusty_read(RSVD_OFFSET) & APMCU_ACK))
 		trusty_write(RSVD_OFFSET, APMCU_ACK);
 
-	dev_dbg(tctx->dev, "exit\n");
+	dev_info(tctx->dev, "%s: exit\n", __func__);
 	return IRQ_HANDLED;
 }
 
