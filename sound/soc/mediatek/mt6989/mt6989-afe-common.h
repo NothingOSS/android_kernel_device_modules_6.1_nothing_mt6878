55c8cfc0a240154d2ad322f4fcb037beae400527
mtk25341 <yu-hsin.wu@mediatek.com>
2023-05-31 15:39:55 +0800

    [ALPS08092809] ULTRA: reopen ultra
    reopen ultra
    add ultra to SKIP_SB
    
    MTK-Commit-Id: 393f29cec794fa45dda3a02aab27963649c6d64a
    
    Signed-off-by: mtk25341 <yu-hsin.wu@mediatek.com>
    CR-Id: ALPS08092809
    Feature: ADSP Audio Framework
    Change-Id: Ie860ef3c5e7f270fcb17e1c9c281f907cabf7536

--- sound/soc/mediatek/mt6989/mt6989-afe-common.h
+++ sound/soc/mediatek/mt6989/mt6989-afe-common.h
@@ -22,12 +22,12 @@
 #define SKIP_SB_BTCVSD
 #define SKIP_SB_OFFLOAD
 #define SKIP_SB_VOW
+#define SKIP_SB_ULTRA
 #define SKIP_SB_AUDIO
 #define SKIP_SMCC_SB
 #else
 /* delete define below if your feature don't want to skip */
 #define SKIP_SB_BTCVSD
-#define SKIP_SB_ULTRA
 #define SKIP_SB_AUDIO
 #endif
 
