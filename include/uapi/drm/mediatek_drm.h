d210f47e09b28a75260765aaa533bcd82bdd3cc0
mtk27633 <xueyan.chen@mediatek.com>
2023-06-13 11:44:43 +0800

    [ALPS08042460] kernel-headers: drm: tdshp reg update
    update tdshp reg according to algo modification
    
    MTK-Commit-Id: 2f3b9973c33dc561e7c7129bc3fe06d3a77c8aa7
    
    Signed-off-by: mtk27633 <xueyan.chen@mediatek.com>
    CR-Id: ALPS08042460
    Feature: display_pq
    Change-Id: Ia3cf5247e6eed2513c049a8e923e2ae33387d52d

--- include/uapi/drm/mediatek_drm.h
+++ include/uapi/drm/mediatek_drm.h
@@ -905,152 +905,183 @@ struct DRM_DISP_WRITE_REG {
 };
 
 struct DISP_TDSHP_REG {
-	__u32 tdshp_softcoring_gain;
-	__u32 tdshp_gain_high;
-	__u32 tdshp_gain_mid;
-	__u32 tdshp_ink_sel;
-	__u32 tdshp_bypass_high;
-	__u32 tdshp_bypass_mid;
-	__u32 tdshp_en;
-	__u32 tdshp_limit_ratio;
-	__u32 tdshp_gain;
-	__u32 tdshp_coring_zero;
-	__u32 tdshp_coring_thr;
-	__u32 tdshp_coring_value;
-	__u32 tdshp_bound;
-	__u32 tdshp_limit;
-	__u32 tdshp_sat_proc;
-	__u32 tdshp_ac_lpf_coe;
-	__u32 tdshp_clip_thr;
-	__u32 tdshp_clip_ratio;
-	__u32 tdshp_clip_en;
-	__u32 tdshp_ylev_p048;
-	__u32 tdshp_ylev_p032;
-	__u32 tdshp_ylev_p016;
-	__u32 tdshp_ylev_p000;
-	__u32 tdshp_ylev_p112;
-	__u32 tdshp_ylev_p096;
-	__u32 tdshp_ylev_p080;
-	__u32 tdshp_ylev_p064;
-	__u32 tdshp_ylev_p176;
-	__u32 tdshp_ylev_p160;
-	__u32 tdshp_ylev_p144;
-	__u32 tdshp_ylev_p128;
-	__u32 tdshp_ylev_p240;
-	__u32 tdshp_ylev_p224;
-	__u32 tdshp_ylev_p208;
-	__u32 tdshp_ylev_p192;
-	__u32 tdshp_ylev_en;
-	__u32 tdshp_ylev_alpha;
-	__u32 tdshp_ylev_256;
-	__u32 pbc1_radius_r;
-	__u32 pbc1_theta_r;
-	__u32 pbc1_rslope_1;
-	__u32 pbc1_gain;
-	__u32 pbc1_lpf_en;
-	__u32 pbc1_en;
-	__u32 pbc1_lpf_gain;
-	__u32 pbc1_tslope;
-	__u32 pbc1_radius_c;
-	__u32 pbc1_theta_c;
-	__u32 pbc1_edge_slope;
-	__u32 pbc1_edge_thr;
-	__u32 pbc1_edge_en;
-	__u32 pbc1_conf_gain;
-	__u32 pbc1_rslope;
-	__u32 pbc2_radius_r;
-	__u32 pbc2_theta_r;
-	__u32 pbc2_rslope_1;
-	__u32 pbc2_gain;
-	__u32 pbc2_lpf_en;
-	__u32 pbc2_en;
-	__u32 pbc2_lpf_gain;
-	__u32 pbc2_tslope;
-	__u32 pbc2_radius_c;
-	__u32 pbc2_theta_c;
-	__u32 pbc2_edge_slope;
-	__u32 pbc2_edge_thr;
-	__u32 pbc2_edge_en;
-	__u32 pbc2_conf_gain;
-	__u32 pbc2_rslope;
-	__u32 pbc3_radius_r;
-	__u32 pbc3_theta_r;
-	__u32 pbc3_rslope_1;
-	__u32 pbc3_gain;
-	__u32 pbc3_lpf_en;
-	__u32 pbc3_en;
-	__u32 pbc3_lpf_gain;
-	__u32 pbc3_tslope;
-	__u32 pbc3_radius_c;
-	__u32 pbc3_theta_c;
-	__u32 pbc3_edge_slope;
-	__u32 pbc3_edge_thr;
-	__u32 pbc3_edge_en;
-	__u32 pbc3_conf_gain;
-	__u32 pbc3_rslope;
-	__u32 tdshp_mid_softlimit_ratio;
-	__u32 tdshp_mid_coring_zero;
-	__u32 tdshp_mid_coring_thr;
-	__u32 tdshp_mid_softcoring_gain;
-	__u32 tdshp_mid_coring_value;
-	__u32 tdshp_mid_bound;
-	__u32 tdshp_mid_limit;
-	__u32 tdshp_high_softlimit_ratio;
-	__u32 tdshp_high_coring_zero;
-	__u32 tdshp_high_coring_thr;
-	__u32 tdshp_high_softcoring_gain;
-	__u32 tdshp_high_coring_value;
-	__u32 tdshp_high_bound;
-	__u32 tdshp_high_limit;
-	__u32 edf_clip_ratio_inc;
-	__u32 edf_edge_gain;
-	__u32 edf_detail_gain;
-	__u32 edf_flat_gain;
-	__u32 edf_gain_en;
-	__u32 edf_edge_th;
-	__u32 edf_detail_fall_th;
-	__u32 edf_detail_rise_th;
-	__u32 edf_flat_th;
-	__u32 edf_edge_slope;
-	__u32 edf_detail_fall_slope;
-	__u32 edf_detail_rise_slope;
-	__u32 edf_flat_slope;
-	__u32 edf_edge_mono_slope;
-	__u32 edf_edge_mono_th;
-	__u32 edf_edge_mag_slope;
-	__u32 edf_edge_mag_th;
-	__u32 edf_edge_trend_flat_mag;
-	__u32 edf_edge_trend_slope;
-	__u32 edf_edge_trend_th;
-	__u32 edf_bld_wgt_mag;
-	__u32 edf_bld_wgt_mono;
-	__u32 edf_bld_wgt_trend;
-	__u32 tdshp_cboost_lmt_u;
-	__u32 tdshp_cboost_lmt_l;
-	__u32 tdshp_cboost_en;
-	__u32 tdshp_cboost_gain;
-	__u32 tdshp_cboost_yconst;
-	__u32 tdshp_cboost_yoffset_sel;
-	__u32 tdshp_cboost_yoffset;
-	__u32 tdshp_post_ylev_p048;
-	__u32 tdshp_post_ylev_p032;
-	__u32 tdshp_post_ylev_p016;
-	__u32 tdshp_post_ylev_p000;
-	__u32 tdshp_post_ylev_p112;
-	__u32 tdshp_post_ylev_p096;
-	__u32 tdshp_post_ylev_p080;
-	__u32 tdshp_post_ylev_p064;
-	__u32 tdshp_post_ylev_p176;
-	__u32 tdshp_post_ylev_p160;
-	__u32 tdshp_post_ylev_p144;
-	__u32 tdshp_post_ylev_p128;
-	__u32 tdshp_post_ylev_p240;
-	__u32 tdshp_post_ylev_p224;
-	__u32 tdshp_post_ylev_p208;
-	__u32 tdshp_post_ylev_p192;
-	__u32 tdshp_post_ylev_en;
-	__u32 tdshp_post_ylev_alpha;
-	__u32 tdshp_post_ylev_256;
+	/* GROUP : MDP_TDSHP_00 (0x0000) */
+	__u32 tdshp_softcoring_gain;         //[MSB:LSB] = [07:00]
+	__u32 tdshp_gain_high;               //[MSB:LSB] = [15:08]
+	__u32 tdshp_gain_mid;                //[MSB:LSB] = [23:16]
+	__u32 tdshp_ink_sel;                 //[MSB:LSB] = [26:24]
+	__u32 tdshp_adap_luma_bp;            //[MSB:LSB] = [28:28]
+	__u32 tdshp_bypass_high;             //[MSB:LSB] = [29:29]
+	__u32 tdshp_bypass_mid;              //[MSB:LSB] = [30:30]
+	__u32 tdshp_en;                      //[MSB:LSB] = [31:31]
+	/* GROUP : MDP_TDSHP_01 (0x0004) */
+	__u32 tdshp_limit_ratio;             //[MSB:LSB] = [03:00]
+	__u32 tdshp_gain;                    //[MSB:LSB] = [11:04]
+	__u32 tdshp_coring_zero;             //[MSB:LSB] = [23:16]
+	__u32 tdshp_coring_thr;              //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_02 (0x0008) */
+	__u32 tdshp_coring_value;            //[MSB:LSB] = [15:08]
+	__u32 tdshp_bound;                   //[MSB:LSB] = [23:16]
+	__u32 tdshp_limit;                   //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_03 (0x000c) */
+	__u32 tdshp_sat_proc;                //[MSB:LSB] = [05:00]
+	__u32 tdshp_ac_lpf_coe;              //[MSB:LSB] = [11:08]
+	__u32 tdshp_clip_thr;                //[MSB:LSB] = [23:16]
+	__u32 tdshp_clip_ratio;              //[MSB:LSB] = [28:24]
+	__u32 tdshp_clip_en;                 //[MSB:LSB] = [31:31]
+	/* GROUP : MDP_TDSHP_05 (0x0014) */
+	__u32 tdshp_ylev_p048;               //[MSB:LSB] = [07:00]
+	__u32 tdshp_ylev_p032;               //[MSB:LSB] = [15:08]
+	__u32 tdshp_ylev_p016;               //[MSB:LSB] = [23:16]
+	__u32 tdshp_ylev_p000;               //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_06 (0x0018) */
+	__u32 tdshp_ylev_p112;               //[MSB:LSB] = [07:00]
+	__u32 tdshp_ylev_p096;               //[MSB:LSB] = [15:08]
+	__u32 tdshp_ylev_p080;               //[MSB:LSB] = [23:16]
+	__u32 tdshp_ylev_p064;               //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_07 (0x001c) */
+	__u32 tdshp_ylev_p176;               //[MSB:LSB] = [07:00]
+	__u32 tdshp_ylev_p160;               //[MSB:LSB] = [15:08]
+	__u32 tdshp_ylev_p144;               //[MSB:LSB] = [23:16]
+	__u32 tdshp_ylev_p128;               //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_08 (0x0020) */
+	__u32 tdshp_ylev_p240;               //[MSB:LSB] = [07:00]
+	__u32 tdshp_ylev_p224;               //[MSB:LSB] = [15:08]
+	__u32 tdshp_ylev_p208;               //[MSB:LSB] = [23:16]
+	__u32 tdshp_ylev_p192;               //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_09 (0x0024) */
+	__u32 tdshp_ylev_en;                 //[MSB:LSB] = [14:14]
+	__u32 tdshp_ylev_alpha;              //[MSB:LSB] = [21:16]
+	__u32 tdshp_ylev_256;                //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_PBC_00 (0x0040) */
+	__u32 pbc1_radius_r;                 //[MSB:LSB] = [05:00]
+	__u32 pbc1_theta_r;                  //[MSB:LSB] = [11:06]
+	__u32 pbc1_rslope_1;                 //[MSB:LSB] = [21:12]
+	__u32 pbc1_gain;                     //[MSB:LSB] = [29:22]
+	__u32 pbc1_lpf_en;                   //[MSB:LSB] = [30:30]
+	__u32 pbc1_en;                       //[MSB:LSB] = [31:31]
+	/* GROUP : MDP_PBC_01 (0x0044) */
+	__u32 pbc1_lpf_gain;                 //[MSB:LSB] = [05:00]
+	__u32 pbc1_tslope;                   //[MSB:LSB] = [15:06]
+	__u32 pbc1_radius_c;                 //[MSB:LSB] = [23:16]
+	__u32 pbc1_theta_c;                  //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_PBC_02 (0x0048) */
+	__u32 pbc1_edge_slope;               //[MSB:LSB] = [05:00]
+	__u32 pbc1_edge_thr;                 //[MSB:LSB] = [13:08]
+	__u32 pbc1_edge_en;                  //[MSB:LSB] = [14:14]
+	__u32 pbc1_conf_gain;                //[MSB:LSB] = [19:16]
+	__u32 pbc1_rslope;                   //[MSB:LSB] = [31:22]
+	/* GROUP : MDP_PBC_03 (0x004c) */
+	__u32 pbc2_radius_r;                 //[MSB:LSB] = [05:00]
+	__u32 pbc2_theta_r;                  //[MSB:LSB] = [11:06]
+	__u32 pbc2_rslope_1;                 //[MSB:LSB] = [21:12]
+	__u32 pbc2_gain;                     //[MSB:LSB] = [29:22]
+	__u32 pbc2_lpf_en;                   //[MSB:LSB] = [30:30]
+	__u32 pbc2_en;                       //[MSB:LSB] = [31:31]
+	/* GROUP : MDP_PBC_04 (0x0050) */
+	__u32 pbc2_lpf_gain;                 //[MSB:LSB] = [05:00]
+	__u32 pbc2_tslope;                   //[MSB:LSB] = [15:06]
+	__u32 pbc2_radius_c;                 //[MSB:LSB] = [23:16]
+	__u32 pbc2_theta_c;                  //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_PBC_05 (0x0054) */
+	__u32 pbc2_edge_slope;               //[MSB:LSB] = [05:00]
+	__u32 pbc2_edge_thr;                 //[MSB:LSB] = [13:08]
+	__u32 pbc2_edge_en;                  //[MSB:LSB] = [14:14]
+	__u32 pbc2_conf_gain;                //[MSB:LSB] = [19:16]
+	__u32 pbc2_rslope;                   //[MSB:LSB] = [31:22]
+	/* GROUP : MDP_PBC_06 (0x0058) */
+	__u32 pbc3_radius_r;                 //[MSB:LSB] = [05:00]
+	__u32 pbc3_theta_r;                  //[MSB:LSB] = [11:06]
+	__u32 pbc3_rslope_1;                 //[MSB:LSB] = [21:12]
+	__u32 pbc3_gain;                     //[MSB:LSB] = [29:22]
+	__u32 pbc3_lpf_en;                   //[MSB:LSB] = [30:30]
+	__u32 pbc3_en;                       //[MSB:LSB] = [31:31]
+	/* GROUP : MDP_PBC_07 (0x005c) */
+	__u32 pbc3_lpf_gain;                 //[MSB:LSB] = [05:00]
+	__u32 pbc3_tslope;                   //[MSB:LSB] = [15:06]
+	__u32 pbc3_radius_c;                 //[MSB:LSB] = [23:16]
+	__u32 pbc3_theta_c;                  //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_PBC_08 (0x0060) */
+	__u32 pbc3_edge_slope;               //[MSB:LSB] = [05:00]
+	__u32 pbc3_edge_thr;                 //[MSB:LSB] = [13:08]
+	__u32 pbc3_edge_en;                  //[MSB:LSB] = [14:14]
+	__u32 pbc3_conf_gain;                //[MSB:LSB] = [19:16]
+	__u32 pbc3_rslope;                   //[MSB:LSB] = [31:22]
+	/* GROUP : MDP_TDSHP_10 (0x0320) */
+	__u32  tdshp_mid_softlimit_ratio;    //[MSB:LSB] = [03:00]
+	__u32  tdshp_mid_coring_zero;        //[MSB:LSB] = [23:16]
+	__u32  tdshp_mid_coring_thr;         //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_11 (0x0324) */
+	__u32  tdshp_mid_softcoring_gain;    //[MSB:LSB] = [07:00]
+	__u32  tdshp_mid_coring_value;       //[MSB:LSB] = [15:08]
+	__u32  tdshp_mid_bound;              //[MSB:LSB] = [13:16]
+	__u32  tdshp_mid_limit;              //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_12 (0x0328) */
+	__u32  tdshp_high_softlimit_ratio;   //[MSB:LSB] = [03:00]
+	__u32  tdshp_high_coring_zero;       //[MSB:LSB] = [23:16]
+	__u32  tdshp_high_coring_thr;        //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_TDSHP_13 (0x032c) */
+	__u32  tdshp_high_softcoring_gain;   //[MSB:LSB] = [07:00]
+	__u32  tdshp_high_coring_value;      //[MSB:LSB] = [15:08]
+	__u32  tdshp_high_bound;             //[MSB:LSB] = [13:16]
+	__u32  tdshp_high_limit;             //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_EDF_GAIN_00 (0x0300) */
+	__u32  edf_clip_ratio_inc;           //[MSB:LSB] = [02:00]
+	__u32  edf_edge_gain;                //[MSB:LSB] = [12:08]
+	__u32  edf_detail_gain;              //[MSB:LSB] = [23:16]
+	__u32  edf_flat_gain;                //[MSB:LSB] = [28:24]
+	__u32  edf_gain_en;                  //[MSB:LSB] = [31:31]
+	/* GROUP : MDP_EDF_GAIN_01 (0x0304) */
+	__u32  edf_edge_th;                  //[MSB:LSB] = [08:00]
+	__u32  edf_detail_fall_th;           //[MSB:LSB] = [17:09]
+	__u32  edf_detail_rise_th;           //[MSB:LSB] = [23:18]
+	__u32  edf_flat_th;                  //[MSB:LSB] = [30:25]
+	/* GROUP : MDP_EDF_GAIN_02 (0x0308) */
+	__u32  edf_edge_slope;               //[MSB:LSB] = [04:00]
+	__u32  edf_detail_fall_slope;        //[MSB:LSB] = [12:08]
+	__u32  edf_detail_rise_slope;        //[MSB:LSB] = [18:16]
+	__u32  edf_flat_slope;               //[MSB:LSB] = [26:24]
+	/* GROUP : MDP_EDF_GAIN_03 (0x030c) */
+	__u32  edf_edge_mono_slope;          //[MSB:LSB] = [03:00]
+	__u32  edf_edge_mono_th;             //[MSB:LSB] = [14:08]
+	__u32  edf_edge_mag_slope;           //[MSB:LSB] = [20:16]
+	__u32  edf_edge_mag_th;              //[MSB:LSB] = [28:24]
+	/* GROUP : MDP_EDF_GAIN_05 (0x0314) */
+	__u32  edf_bld_wgt_mag;              //[MSB:LSB] = [07:00]
+	__u32  edf_bld_wgt_mono;             //[MSB:LSB] = [15:08]
+	/* GROUP : MDP_C_BOOST_MAIN (0x00e0) */
+	__u32  tdshp_cboost_gain;            //[MSB:LSB] = [07:00]
+	__u32  tdshp_cboost_en;              //[MSB:LSB] = [13:13]
+	__u32  tdshp_cboost_lmt_l;           //[MSB:LSB] = [23:16]
+	__u32  tdshp_cboost_lmt_u;           //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_C_BOOST_MAIN_2 (0x00e4) */
+	__u32  tdshp_cboost_yoffset;         //[MSB:LSB] = [06:00]
+	__u32  tdshp_cboost_yoffset_sel;     //[MSB:LSB] = [17:16]
+	__u32  tdshp_cboost_yconst;          //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_POST_YLEV_00 (0x0480) */
+	__u32 tdshp_post_ylev_p048;          //[MSB:LSB] = [07:00]
+	__u32 tdshp_post_ylev_p032;          //[MSB:LSB] = [13:13]
+	__u32 tdshp_post_ylev_p016;          //[MSB:LSB] = [23:16]
+	__u32 tdshp_post_ylev_p000;          //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_POST_YLEV_01 (0x0484) */
+	__u32 tdshp_post_ylev_p112;          //[MSB:LSB] = [07:00]
+	__u32 tdshp_post_ylev_p096;          //[MSB:LSB] = [13:13]
+	__u32 tdshp_post_ylev_p080;          //[MSB:LSB] = [23:16]
+	__u32 tdshp_post_ylev_p064;          //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_POST_YLEV_02 (0x0488) */
+	__u32 tdshp_post_ylev_p176;          //[MSB:LSB] = [07:00]
+	__u32 tdshp_post_ylev_p160;          //[MSB:LSB] = [13:13]
+	__u32 tdshp_post_ylev_p144;          //[MSB:LSB] = [23:16]
+	__u32 tdshp_post_ylev_p128;          //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_POST_YLEV_03 (0x048c) */
+	__u32 tdshp_post_ylev_p240;          //[MSB:LSB] = [07:00]
+	__u32 tdshp_post_ylev_p224;          //[MSB:LSB] = [13:13]
+	__u32 tdshp_post_ylev_p208;          //[MSB:LSB] = [23:16]
+	__u32 tdshp_post_ylev_p192;          //[MSB:LSB] = [31:24]
+	/* GROUP : MDP_POST_YLEV_04 (0x0490) */
+	__u32 tdshp_post_ylev_en;            //[MSB:LSB] = [14:14]
+	__u32 tdshp_post_ylev_alpha;         //[MSB:LSB] = [21:16]
+	__u32 tdshp_post_ylev_256;           //[MSB:LSB] = [31:24]
 };
 
 struct DISP_TDSHP_DISPLAY_SIZE {
