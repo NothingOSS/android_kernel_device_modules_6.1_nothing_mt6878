713e0ccb137fe7bcdee104c30755053d853cb623
Greg Kroah-Hartman <gregkh@google.com>
2023-06-13 11:41:31 +0800

    [ALPS08137807] ANDROID: preserve CRC for xhci symbols
    In release 6.1.24 a new .h file was included in the xhci code, which
    caused the CRCs to change as some structures changed into "real"
    structures instead of anonymous definitions.  So preserve the CRCs by
    commenting out the #include if GENKSYMS is being calculated.
    
    This will be removed the next KABI break, as it shouldnt be sticking
    around long.
    
    MTK-Commit-Id: 2daa35d96037aee22ccf4dae4f8e1e1003d38c9e
    
    Bug: 161946584
    Fixes: 167c05646f67 ("xhci: also avoid the XHCI_ZERO_64B_REGS quirk with a passthrough iommu")
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I59838b0af869d3e17fc73d72eb473190c50281fd
    Signed-off-by: Chih-Chun Wang <Chih-Chun.Wang@mediatek.com>
    CR-Id: ALPS08137807
    Feature: usb_ssusb

--- drivers/misc/mediatek/usb/usb_xhci/xhci.c
+++ drivers/misc/mediatek/usb/usb_xhci/xhci.c
@@ -10,7 +10,9 @@
  */
 
 #include <linux/pci.h>
+#ifndef __GENKSYMS__	/* ANDROID: KABI CRC preservation hack */
 #include <linux/iommu.h>
+#endif
 #include <linux/iopoll.h>
 #include <linux/irq.h>
 #include <linux/log2.h>
