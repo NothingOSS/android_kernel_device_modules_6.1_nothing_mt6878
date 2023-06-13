2debd4b354ef381110ff5ac6467fde0451dc9671
Sean Lin <yu-syuan.Lin@mediatek.com>
2023-06-13 11:45:14 +0800

    [ALPS08136507] modify jpgenc afbc size calculation
    afbc buffer must 32 alignment
    
    MTK-Commit-Id: 61f96a323e5fc168de1c4e2cff53333b8b2e2366
    
    Signed-off-by: Sean Lin <yu-syuan.Lin@mediatek.com>
    CR-Id: ALPS08136507
    Feature: Video HAL
    Change-Id: I26a9e48196beaf96256dbadb282bc53ecf339662

--- drivers/media/platform/mtk-jpeg/mtk_jpeg_core.c
+++ drivers/media/platform/mtk-jpeg/mtk_jpeg_core.c
@@ -318,9 +318,11 @@ static int mtk_jpeg_try_fmt_mplane(struct v4l2_pix_format_mplane *pix_mp,
 			unsigned int block_w = 16;
 			unsigned int block_h = 16;
 			unsigned int bitsPP = 12;
+			unsigned int width = round_up(pix_mp->width, 32);
+			unsigned int height = round_up(pix_mp->height, 32);
 			unsigned int block_count =
-					((pix_mp->width + (block_w - 1))/block_w)
-					*((pix_mp->height + (block_h - 1))/block_h);
+					((width + (block_w - 1))/block_w)
+					*((height + (block_h - 1))/block_h);
 			pfmt->bytesperline = stride;
 			pfmt->sizeimage = (block_count << 4) +
 					(block_count * block_w * block_h  * bitsPP / 8);
