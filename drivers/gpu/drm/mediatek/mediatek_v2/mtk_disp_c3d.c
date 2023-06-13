1b7f04589f13a59f2c7d0c0aeb6ef46887825a4b
mtk08122 <shan.an@mediatek.com>
2023-06-13 11:47:05 +0800

    [ALPS08149512] c3d: force stop gce thread
    cmdq_mbox_disable would free cmdq related power/cg,
    so cmdq pkt should be stop before cmdq_mbox_diable
    
    MTK-Commit-Id: 7fc528dfc567c18c04b814eda8eca213b5378e34
    
    CR-Id: ALPS08149512
    Feature: display_pq
    Signed-off-by: mtk08122 <shan.an@mediatek.com>
    Change-Id: Ie637714dc84daabe158bc530da26e7db416b5021

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_c3d.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_disp_c3d.c
@@ -199,14 +199,17 @@ static bool disp_c3d_write_sram(struct mtk_ddp_comp *comp, int cmd_type)
 	case C3D_USERSPACE:
 	case C3D_FIRST_ENABLE:
 		cmdq_pkt_flush(cmdq_handle);
-		if (async == false)
+		if (async == false) {
+			cmdq_mbox_stop(client);
 			cmdq_mbox_disable(client->chan);
+		}
 		break;
 
 	case C3D_PREPARE:
 		cmdq_pkt_refinalize(cmdq_handle);
 		if (async == false) {
 			cmdq_pkt_flush(cmdq_handle);
+			cmdq_mbox_stop(client);
 			cmdq_mbox_disable(client->chan);
 		} else {
 			int ret = 0;
