d6444cbbbfcc54527da75c95ad7053c73b54220c
jim.tsai <Wei-chin.Tsai@mediatek.com>
2023-06-13 11:43:32 +0800

    [ALPS08001469] keep the last 128 record suspendinfo
    - Always keep the latest 128 suspend info all the time
    
    MTK-Commit-Id: 8d75183f45e55813cc9e5084f62d15198793eb4b
    
    Signed-off-by: jim.tsai <Wei-chin.Tsai@mediatek.com>
    CR-Id: ALPS08001469
    Feature: MBrain
    Change-Id: I5e5480a5a03f351105ef8b9e5ae4ccb4c97f0850

--- drivers/misc/mediatek/mbraink/mbraink_suspend_info.c
+++ drivers/misc/mediatek/mbraink/mbraink_suspend_info.c
@@ -57,46 +57,36 @@ void mbraink_set_suspend_info_list_record(unsigned short datatype)
 
 	w_idx = (unsigned short)(mbraink_suspend_info_list_p_data.w_idx % SUSPEND_INFO_SZ);
 
-	if (mbraink_suspend_info_list_data[w_idx].dirty == false) {
-		ktime_get_real_ts64(&tv);
-		mbraink_suspend_info_list_data[w_idx].timestamp =
-			(tv.tv_sec*1000)+(tv.tv_nsec/1000000);
-		mbraink_suspend_info_list_data[w_idx].datatype =
-			datatype;
-		if (datatype == 1) {
-			last_resume_reason_show(NULL, NULL, buf);
-			if (strstr(buf, "Abort:"))
-				mbraink_suspend_info_list_data[w_idx].reason = -2;
-			else if (strstr(buf, "-1"))
-				mbraink_suspend_info_list_data[w_idx].reason = -1;
-			else {
-				int irq = -3, size = 0;
+	ktime_get_real_ts64(&tv);
+	mbraink_suspend_info_list_data[w_idx].timestamp =
+		(tv.tv_sec*1000)+(tv.tv_nsec/1000000);
+	mbraink_suspend_info_list_data[w_idx].datatype =
+		datatype;
+	if (datatype == 1) {
+		last_resume_reason_show(NULL, NULL, buf);
+		if (strstr(buf, "Abort:"))
+			mbraink_suspend_info_list_data[w_idx].reason = -2;
+		else if (strstr(buf, "-1"))
+			mbraink_suspend_info_list_data[w_idx].reason = -1;
+		else {
+			int irq = -3, size = 0;
 
-				size = sscanf(buf, "%d %s\n", &irq, sub_buf);
-				if (size < 0)
-					pr_info("%s, sscanf failed\n", __func__);
-				mbraink_suspend_info_list_data[w_idx].reason = irq;
-			}
-		} else
-			mbraink_suspend_info_list_data[w_idx].reason = 0;
-		pr_info("%s: w_idx = %u, timestamp=%lld, datatype=%u, reason=%d\n",
-			__func__,
-			w_idx,
-			mbraink_suspend_info_list_data[w_idx].timestamp,
-			mbraink_suspend_info_list_data[w_idx].datatype,
-			mbraink_suspend_info_list_data[w_idx].reason);
-		mbraink_suspend_info_list_data[w_idx].dirty = true;
-		mbraink_suspend_info_list_p_data.w_idx =
-			(mbraink_suspend_info_list_p_data.w_idx + 1) % SUSPEND_INFO_SZ;
-	} else {
-		pr_notice("buffer is full,  w_idx = %u !!!\n",
-			w_idx);
-		memset(mbraink_suspend_info_list_data,
-			0,
-			SUSPEND_INFO_SZ * sizeof(struct mbraink_suspend_info_list));
-		mbraink_suspend_info_list_p_data.r_idx = 0;
-		mbraink_suspend_info_list_p_data.w_idx = 0;
-	}
+			size = sscanf(buf, "%d %s\n", &irq, sub_buf);
+			if (size < 0)
+				pr_info("%s, sscanf failed\n", __func__);
+			mbraink_suspend_info_list_data[w_idx].reason = irq;
+		}
+	} else
+		mbraink_suspend_info_list_data[w_idx].reason = 0;
+	pr_info("%s: w_idx = %u, timestamp=%lld, datatype=%u, reason=%d\n",
+		__func__,
+		w_idx,
+		mbraink_suspend_info_list_data[w_idx].timestamp,
+		mbraink_suspend_info_list_data[w_idx].datatype,
+		mbraink_suspend_info_list_data[w_idx].reason);
+	mbraink_suspend_info_list_data[w_idx].dirty = true;
+	mbraink_suspend_info_list_p_data.w_idx =
+		(mbraink_suspend_info_list_p_data.w_idx + 1) % SUSPEND_INFO_SZ;
 
 	spin_unlock_irqrestore(&suspend_info_list_lock, flags);
 
@@ -110,13 +100,12 @@ void mbraink_get_suspend_info_list_record(struct mbraink_suspend_info_struct_dat
 {
 	unsigned int buf_idx = 0;
 	unsigned short r_idx = 0;
-	unsigned short init_r_idx = 0;
 	unsigned long flags;
 
 	spin_lock_irqsave(&suspend_info_list_lock, flags);
 
 	memset(buffer, 0, sizeof(struct mbraink_suspend_info_struct_data));
-	init_r_idx = r_idx = mbraink_suspend_info_list_p_data.r_idx % SUSPEND_INFO_SZ;
+	r_idx = mbraink_suspend_info_list_p_data.r_idx % SUSPEND_INFO_SZ;
 
 	do {
 		if (buf_idx >= max) {
@@ -137,12 +126,10 @@ void mbraink_get_suspend_info_list_record(struct mbraink_suspend_info_struct_dat
 			mbraink_suspend_info_list_data[r_idx].dirty = false;
 			buf_idx++;
 			buffer->count++;
-		} else
-			break;
-
-		r_idx = (r_idx + 1) % SUSPEND_INFO_SZ;
-	} while (r_idx != init_r_idx);
-	mbraink_suspend_info_list_p_data.r_idx = r_idx;
+		}
+		r_idx++;
+	} while (r_idx < SUSPEND_INFO_SZ);
+	mbraink_suspend_info_list_p_data.r_idx = r_idx % SUSPEND_INFO_SZ;
 	spin_unlock_irqrestore(&suspend_info_list_lock, flags);
 }
 
