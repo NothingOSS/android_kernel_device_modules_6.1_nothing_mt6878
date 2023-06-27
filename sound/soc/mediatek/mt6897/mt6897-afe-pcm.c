9ee35f2199ce1417c9f868eaa223b3f080e7b2bd
chien-wei hsu <chien-wei.hsu@mediatek.com>
2023-06-27 09:07:17 +0800

    [ALPS08136625] audio_dsp: warning the abnormal flow of dsp_mem
    General flow:
    set use_adsp_share_mem 1-> open pcm
    -> ... ->
    close pcm -> set use_adsp_share_mem 0
    
    Abnormal flow:
    set use_adsp_share_mem 1-> open pcm
    -> ... ->
    set use_adsp_share_mem 0 -> close pcm
    
    If the use_adsp_share_mem set to 0 before pcm close, it may cause
    the hw_free message cant send to adsp.
    
    Add AUDIO_AEE() when this case happened.
    
    MTK-Commit-Id: 363040b1680ba8fda62e4da321ec33aef8a10061
    
    Signed-off-by: chien-wei hsu <chien-wei.hsu@mediatek.com>
    CR-Id: ALPS08136625
    Feature: alsa_sound_dsp
    Change-Id: I608f19e83f07d6d3ce4fe2ed50c9514b349a8532

--- sound/soc/mediatek/mt6897/mt6897-afe-pcm.c
+++ sound/soc/mediatek/mt6897/mt6897-afe-pcm.c
@@ -1226,6 +1226,7 @@ static int mt6897_adsp_mem_set(struct snd_kcontrol *kcontrol,
 	int ul_memif_num = -1;
 	int ref_memif_num = -1;
 	int task_id = get_dsp_task_id_from_str(kcontrol->id.name);
+	int old_value;
 
 	switch (task_id) {
 	case AUDIO_TASK_PRIMARY_ID:
@@ -1245,14 +1246,9 @@ static int mt6897_adsp_mem_set(struct snd_kcontrol *kcontrol,
 	case AUDIO_TASK_ECHO_REF_ID:
 	case AUDIO_TASK_USBUL_ID:
 	case AUDIO_TASK_MDDL_ID:
-		ul_memif_num = get_dsp_task_attr(task_id,
-						 ADSP_TASK_ATTR_MEMUL);
-		break;
 	case AUDIO_TASK_BTUL_ID:
 		ul_memif_num = get_dsp_task_attr(task_id,
 						 ADSP_TASK_ATTR_MEMUL);
-		ref_memif_num = get_dsp_task_attr(task_id,
-						 ADSP_TASK_ATTR_MEMREF);
 		break;
 	case AUDIO_TASK_CALL_FINAL_ID:
 	case AUDIO_TASK_PLAYBACK_ID:
@@ -1272,17 +1268,38 @@ static int mt6897_adsp_mem_set(struct snd_kcontrol *kcontrol,
 
 	if (dl_memif_num >= 0) {
 		memif = &afe->memif[dl_memif_num];
+		old_value = memif->use_adsp_share_mem;
 		memif->use_adsp_share_mem = ucontrol->value.integer.value[0];
+
+		pr_info("%s(), dl:%s, use_adsp_share_mem %d->%d\n",
+			__func__, memif->data->name, old_value, memif->use_adsp_share_mem);
+
+		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value)
+			AUDIO_AEE("disable adsp memory used before substream shutdown");
 	}
 
 	if (ul_memif_num >= 0) {
 		memif = &afe->memif[ul_memif_num];
+		old_value = memif->use_adsp_share_mem;
 		memif->use_adsp_share_mem = ucontrol->value.integer.value[0];
+
+		pr_info("%s(), ul:%s, use_adsp_share_mem %d->%d\n",
+			__func__, memif->data->name, old_value, memif->use_adsp_share_mem);
+
+		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value)
+			AUDIO_AEE("disable adsp memory used before substream shutdown");
 	}
 
 	if (ref_memif_num >= 0) {
 		memif = &afe->memif[ref_memif_num];
+		old_value = memif->use_adsp_share_mem;
 		memif->use_adsp_share_mem = ucontrol->value.integer.value[0];
+
+		pr_info("%s(), ref:%s, use_adsp_share_mem %d->%d\n",
+			__func__, memif->data->name, old_value, memif->use_adsp_share_mem);
+
+		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value)
+			AUDIO_AEE("disable adsp memory used before substream shutdown");
 	}
 
 	return 0;
