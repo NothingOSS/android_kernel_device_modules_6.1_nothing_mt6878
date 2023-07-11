36a71d639b29aa9e6d7d75447f85b091148c53f2
Chris-YC Chen <chris-yc.chen@mediatek.com>
2023-07-11 15:50:43 +0800

    [ALPS08202400] mml: enable DPC
    enable DPC
    
    MTK-Commit-Id: 7d41db08424acada48dd05bfc824f002c15105cb
    
    Signed-off-by: Chris-YC Chen <chris-yc.chen@mediatek.com>
    CR-Id: ALPS08202400
    Feature: mml
    Change-Id: I1e4b8c4fc71a03914631a9eb92dfffa81f9dfeb1

--- drivers/gpu/drm/mediatek/mml/mtk-mml-tp-mt6989.c
+++ drivers/gpu/drm/mediatek/mml/mtk-mml-tp-mt6989.c
@@ -58,7 +58,7 @@ module_param(mml_racing_rsz, int, 0644);
 int mml_need_irq;
 module_param(mml_need_irq, int, 0644);
 
-int mml_dpc;
+int mml_dpc = 1;
 module_param(mml_dpc, int, 0644);
 
 /* 0: off
