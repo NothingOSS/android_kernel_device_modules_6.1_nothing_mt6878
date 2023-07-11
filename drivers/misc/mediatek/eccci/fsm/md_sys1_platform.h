f534f9a190c0bd57d409156a722ef66d871d9408
zhaohui.yu <zhaohui.yu@mediatek.com>
2023-07-11 15:50:42 +0800

    [ALPS08216937] [revert]ccci: add pmic information dump
    Add pmic information dump, when md ee happen.
    MD team need pmic dump, so add it as general flow.
    This dump flow is common code and not by project.
    
    MTK-Commit-Id: 152a81ac823d8a6e6ae7dbff8aaf9ec6796037c6
    
    Signed-off-by: zhaohui.yu <zhaohui.yu@mediatek.com>
    CR-Id: ALPS08216937
    Feature: CCCI_HIF_CONTROL
    Change-Id: I5c4a9dfec0f55ba1c57d91badd9afecc2964a270

--- drivers/misc/mediatek/eccci/fsm/md_sys1_platform.h
+++ drivers/misc/mediatek/eccci/fsm/md_sys1_platform.h
@@ -117,5 +117,4 @@ extern void mtk_emidbg_dump(void);
 extern unsigned int ccci_get_hs2_done_status(void);
 extern void reset_modem_hs2_status(void);
 
-extern void spmi_dump_pmif_record_reg(void);
 #endif				/* __MD_SYS1_PLATFORM_H__ */
