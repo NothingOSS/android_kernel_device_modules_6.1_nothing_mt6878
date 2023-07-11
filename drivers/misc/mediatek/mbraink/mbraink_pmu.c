b77b777dd85b2567e37d05ddc9ae3ea1b460439c
Raymond Lin <yu-jui.lin@mediatek.com>
2023-07-11 15:47:43 +0800

    [ALPS08193511] mbraink pmu support ponsot
    - mbraink pmu support ponsot
    - compatitable for leroy by dts
    
    MTK-Commit-Id: 8726939e6cf6c37c838cb87474289e2fd281f3ad
    
    Signed-off-by: Raymond Lin <yu-jui.lin@mediatek.com>
    CR-Id: ALPS08193511
    Feature: MBrain
    Change-Id: I7c49c5fa81af95b9376a1110eec9a254fa10efc7

--- drivers/misc/mediatek/mbraink/mbraink_pmu.c
+++ drivers/misc/mediatek/mbraink/mbraink_pmu.c
@@ -8,11 +8,17 @@
 #include <linux/slab.h>
 #include <linux/time.h>
 #include <linux/timer.h>
+#include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
 #include "mbraink_pmu.h"
 
-#define CPU_INST_SPEC_OFFSET	0x20
-#define max_cpus 8
+#define MBRAIN_ENABLE_ADDR_OFFSET    0x1330
 #define CPU_INST_SPEC_UPDATE_INTEVAL   10   // ex: 0.1 * HZ
+
+bool mbraink_pmu_dts_exist;
+u32 cpu_inst_spec_offset = 0x1274;  // 0x1254 + 0x20 for Ponsot and before
 
 static DEFINE_PER_CPU(u64, cpu_last_inst_spec);
 static u64 cpu0_pmu_data_inst_spec;
@@ -24,8 +30,8 @@ static u64 cpu5_pmu_data_inst_spec;
 static u64 cpu6_pmu_data_inst_spec;
 static u64 cpu7_pmu_data_inst_spec;
 
-static void __iomem *general_tcm_base_addr;
-static void __iomem *pmu_data_tcm_base_addr;
+static void __iomem *csram_base;
+static void __iomem *pmu_tcm_base;
 
 static struct timer_list timer_inst_spec;
 
@@ -74,14 +80,29 @@ void uninit_pmu_keep_data(void)
 #endif
 }
 
+static void set_pmu_enable(unsigned int enable)
+{
+	if (IS_ERR_OR_NULL((void *)csram_base))
+		return;
+
+	__raw_writel((u32)enable, csram_base + MBRAIN_ENABLE_ADDR_OFFSET);
+	/* make sure register access in order */
+	wmb();
+}
+
 u64 get_cpu_pmu(int cpu, u32 offset)
 {
 	u64 count = 0;
 
-	if (IS_ERR_OR_NULL((void *)pmu_data_tcm_base_addr))
-		return count;
-
-	count = __raw_readl(pmu_data_tcm_base_addr + offset + (cpu * 0x4));
+	if (mbraink_pmu_dts_exist) {
+		if (IS_ERR_OR_NULL((void *)pmu_tcm_base))
+			return count;
+		count = __raw_readl(pmu_tcm_base + offset + (cpu * 0x4));
+	} else {
+		if (IS_ERR_OR_NULL((void *)csram_base))
+			return count;
+		count = __raw_readl(csram_base + offset + (cpu * 0x4));
+	}
 	return count;
 }
 
@@ -120,23 +141,124 @@ u64 get_cur_cpu_##_name(int cpu)							\
 		return res;									\
 }
 
-DEFINE_GET_CUR_CPU_PMU_FUNC(inst_spec, CPU_INST_SPEC_OFFSET);
+DEFINE_GET_CUR_CPU_PMU_FUNC(inst_spec, cpu_inst_spec_offset);
+
+bool is_mbraink_pmu_dts_exist(void)
+{
+	struct device_node *mbraink_pmu_node;
+	bool ret = false;
+
+	mbraink_pmu_node = of_find_node_by_name(NULL, "mbraink-pmu-info");
+	if (!mbraink_pmu_node) {
+		pr_info("failed to find node @ %s\n", __func__);
+		ret = false;
+	} else {
+		pr_info("mbraink-pmu-info node found.\n");
+		ret = true;
+	}
+	return ret;
+}
+
+u32 get_mbraink_pmu_dts_property(const char *property_name)
+{
+	struct device_node *mbraink_pmu_node;
+	u32 para = 0;
+
+	mbraink_pmu_node = of_find_node_by_name(NULL, "mbraink-pmu-info");
+	if (mbraink_pmu_node == NULL)
+		pr_info("failed to find node @ %s\n", __func__);
+	else {
+		int ret;
+
+		ret = of_property_read_u32(mbraink_pmu_node, property_name, &para);
+		if (ret < 0)
+			pr_info("no %s dts_ret=%d\n", property_name, ret);
+		else
+			pr_info("%s enabled\n", property_name);
+	}
+
+	return para;
+}
 
 #if IS_ENABLED(CONFIG_MTK_LPM_MT6989)
 
 int mbraink_pmu_init(void)
 {
+	int ret = 0;
+	u32 cdfv_tcm_base_start = 0, pmu_tcm_base_start = 0;
+	struct device_node *dn = NULL;
+	struct platform_device *pdev = NULL;
+	struct resource *csram_res = NULL;
+
 	pr_notice("mbraink pmu init.\n");
-	general_tcm_base_addr = ioremap(GENERAL_TCM_ADDR, GENERAL_TCM_SIZE);
-	pmu_data_tcm_base_addr = ioremap(PMU_DATA_TCM_ADDR, PMU_DATA_TCM_SIZE);
-	return 0;
+	csram_base = NULL;
+	pmu_tcm_base = NULL;
+	mbraink_pmu_dts_exist = is_mbraink_pmu_dts_exist();
+	if (mbraink_pmu_dts_exist) {
+		cpu_inst_spec_offset = get_mbraink_pmu_dts_property("cpu-inst-spec-offset");
+		cdfv_tcm_base_start = get_mbraink_pmu_dts_property("cdfv-tcm-base");
+		if (cdfv_tcm_base_start == 0) {
+			ret = -ENODEV;
+			pr_info("%s: find cdfv-tcm-base from dts failed\n", __func__);
+			goto get_base_failed;
+		} else {
+			csram_base = ioremap(cdfv_tcm_base_start, get_mbraink_pmu_dts_property("cdfv-tcm-base-len"));
+		}
+
+		pmu_tcm_base_start = get_mbraink_pmu_dts_property("pmu-tcm-base");
+		if (pmu_tcm_base_start == 0) {
+			ret = -ENODEV;
+			pr_info("%s: find pmu-tcm-base from dts failed\n", __func__);
+			goto get_base_failed;
+		} else {
+			pmu_tcm_base = ioremap(pmu_tcm_base_start, get_mbraink_pmu_dts_property("pmu-tcm-base-len"));
+		}
+	} else {
+		/* get cpufreq driver base address */
+		dn = of_find_node_by_name(NULL, "cpuhvfs");
+		if (!dn) {
+			ret = -ENOMEM;
+			pr_info("%s: find cpuhvfs node failed\n", __func__);
+			goto get_base_failed;
+		}
+
+		pdev = of_find_device_by_node(dn);
+		of_node_put(dn);
+		if (!pdev) {
+			ret = -ENODEV;
+			pr_info("%s: cpuhvfs is not ready\n", __func__);
+			goto get_base_failed;
+		}
+
+		csram_res = platform_get_resource(pdev, IORESOURCE_MEM, 1);
+		if (!csram_res) {
+			ret = -ENODEV;
+			pr_info("%s: cpuhvfs resource is not found\n", __func__);
+			goto get_base_failed;
+		}
+
+		csram_base = ioremap(csram_res->start, resource_size(csram_res));
+		if (IS_ERR_OR_NULL((void *)csram_base)) {
+			ret = -ENOMEM;
+			pr_info("%s: find csram base failed\n", __func__);
+			goto get_base_failed;
+		}
+	}
+get_base_failed:
+	return ret;
 }
 
 int mbraink_pmu_uninit(void)
 {
 	pr_notice("mbraink pmu uninit.\n");
-	iounmap(general_tcm_base_addr);
-	iounmap(pmu_data_tcm_base_addr);
+	if (!csram_base) {
+		iounmap(csram_base);
+		csram_base = NULL;
+	}
+	if (!pmu_tcm_base) {
+		iounmap(pmu_tcm_base);
+		pmu_tcm_base = NULL;
+	}
 	return 0;
 }
 
@@ -153,10 +275,10 @@ int mbraink_enable_pmu_inst_spec(bool enable)
 		cpu6_pmu_data_inst_spec = 0;
 		cpu7_pmu_data_inst_spec = 0;
 		init_pmu_keep_data();
-		iowrite32(1, general_tcm_base_addr+MBRAIN_ENABLE_TCM_OFFSET);
+		set_pmu_enable(1);
 	} else {
 		uninit_pmu_keep_data();
-		iowrite32(0, general_tcm_base_addr+MBRAIN_ENABLE_TCM_OFFSET);
+		set_pmu_enable(0);
 	}
 	return 0;
 }
@@ -198,6 +320,7 @@ int mbraink_get_pmu_inst_spec(struct mbraink_pmu_info *pmuInfo)
 int mbraink_pmu_init(void)
 {
 	pr_info("%s: not enable do nothing\n", __func__);
+	mbraink_pmu_dts_exist = false;
 	return 0;
 }
 
