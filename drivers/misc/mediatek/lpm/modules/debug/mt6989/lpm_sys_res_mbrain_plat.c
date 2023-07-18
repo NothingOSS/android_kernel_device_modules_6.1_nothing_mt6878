194761cf737e5daedd31ee32e37ca5afc2f8cf28
jia-xun.li <jia-xun.li@mediatek.com>
2023-07-18 10:45:43 +0800

    [ALPS08217082] fix coverity
    CERT-C Expression fix
    
    MTK-Commit-Id: 38663cf7054a2eeaa3b5be34896bdd2ea91ae321
    
    Signed-off-by: jia-xun.li <jia-xun.li@mediatek.com>
    CR-Id: ALPS08217082
    Feature: lpm
    Change-Id: Iec4818ef2ca0ee7d47cca773b8205309b1890d20

--- drivers/misc/mediatek/lpm/modules/debug/mt6989/lpm_sys_res_mbrain_plat.c
+++ drivers/misc/mediatek/lpm/modules/debug/mt6989/lpm_sys_res_mbrain_plat.c
@@ -34,8 +34,8 @@ void get_sys_res_header(void)
 {
 	int i = 0;
 
-	for(i=0; i<NR_SPM_GRP; i++) {
-		if(group_release[i]) {
+	for (i=0; i<NR_SPM_GRP; i++) {
+		if (group_release[i]) {
 			sys_res_sig_num += 1;
 			sys_res_sig_num += sys_res_group_info[i].group_num;
 		}
@@ -70,21 +70,29 @@ int lpm_mbrain_get_sys_res_data(void *address, uint32_t size)
 	struct sys_res_scene_info scene_info;
 	void *sig_info;
 
-	if(header.index_data_length == 0)
-		ret = -1;
-
-	if(address && size < header.index_data_length + header.data_offset)
+	if (!address ||
+	    header.index_data_length == 0 ||
+	    size < header.index_data_length + header.data_offset)
 		ret = -1;
 
 	/* Copy header */
 	address = sys_res_data_copy(address, &header, sizeof(struct sys_res_mbrain_header));
 
 	sys_res_ops = get_lpm_sys_res_ops();
-	if (sys_res_ops && sys_res_ops->update) {
-		spin_lock_irqsave(&sys_res_ops->lock, flag);
-		sys_res_ops->update();
-		spin_unlock_irqrestore(&sys_res_ops->lock, flag);
+	if (!sys_res_ops ||
+	    !sys_res_ops->update ||
+	    !sys_res_ops->get_last_suspend ||
+	    !sys_res_ops->get_detail) {
+		pr_info("[name:spm&][SPM] Get sys res operations fail\n");
+		ret = -1;
 	}
+
+	if (ret)
+		return ret;
+
+	spin_lock_irqsave(&sys_res_ops->lock, flag);
+	sys_res_ops->update();
+	spin_unlock_irqrestore(&sys_res_ops->lock, flag);
 
 	/* Copy scenario data */
 	sys_res_record[SYS_RES_RELEASE_SCENE_COMMON] = sys_res_ops->get(SYS_RES_SCENE_COMMON);
@@ -93,7 +101,7 @@ int lpm_mbrain_get_sys_res_data(void *address, uint32_t size)
 
 	spin_lock_irqsave(&sys_res_ops->lock, flag);
 	scene_info.res_sig_num = sys_res_sig_num;
-	for(i=0; i<SCENE_RELEASE_NUM; i++) {
+	for (i=0; i<SCENE_RELEASE_NUM; i++) {
 		scene_info.duration_time = sys_res_ops->get_detail(sys_res_record[i],
 								SYS_RES_DURATION, 0);
 		scene_info.suspend_time = sys_res_ops->get_detail(sys_res_record[i],
@@ -103,13 +111,13 @@ int lpm_mbrain_get_sys_res_data(void *address, uint32_t size)
 	}
 
 	/* Copy signal data */
-	for(i=0; i<SCENE_RELEASE_NUM; i++) {
+	for (i=0; i<SCENE_RELEASE_NUM; i++) {
 		for (j = 0; j <= VCORE_REQ; j++){
-			if(group_release[j]) {
+			if (group_release[j]) {
 				sig_info = (void *)sys_res_ops->get_detail(sys_res_record[i],
 							SYS_RES_SIG_ADDR,
 							sys_res_group_info[j].sys_index);
-				if(sig_info)
+				if (sig_info)
 					address = sys_res_data_copy(address,
 								sig_info,
 								sizeof(struct sys_res_sig_info));
@@ -117,7 +125,7 @@ int lpm_mbrain_get_sys_res_data(void *address, uint32_t size)
 				sig_info = (void *)sys_res_ops->get_detail(sys_res_record[i],
 							SYS_RES_SIG_ADDR,
 							sys_res_group_info[j].sig_table_index);
-				if(sig_info)
+				if (sig_info)
 					address = sys_res_data_copy(address,
 								sig_info,
 								sizeof(struct sys_res_sig_info) *
