e4e37f69ae672cc3bde7085b346f712af565fff9
Yiwen Chiou <yiwen.chiou@mediatek.com>
2023-08-01 14:24:03 +0800

    [ALPS08239838] ASoC: Enable abnormal dsp_mem debug
        Enable abnormal dsp_mem debug
    
    MTK-Commit-Id: 4e43abd8916b4d098d5030e9df937b42f9c14ce8
    
    Signed-off-by: Yiwen Chiou <yiwen.chiou@mediatek.com>
    CR-Id: ALPS08239838
    Feature: alsa_sound_soc
    Change-Id: I92325fe5a451a53090fc1ab320503e672bec9bd3

--- sound/soc/mediatek/mt6989/mt6989-afe-pcm.c
+++ sound/soc/mediatek/mt6989/mt6989-afe-pcm.c
@@ -114,6 +114,11 @@ void mt6989_fe_shutdown(struct snd_pcm_substream *substream,
 	struct mtk_base_afe_memif *memif = &afe->memif[memif_num];
 	int irq_id = memif->irq_usage;
 
+	if (memif->err_close_order) {
+		dump_stack();
+		memif->err_close_order = false;
+	}
+
 	memif->substream = NULL;
 	afe_priv->irq_cnt[memif_num] = 0;
 	afe_priv->xrun_assert[memif_num] = 0;
@@ -1288,8 +1293,10 @@ static int mt6989_adsp_mem_set(struct snd_kcontrol *kcontrol,
 		pr_info("%s(), dl:%s, use_adsp_share_mem %d->%d\n",
 			__func__, memif->data->name, old_value, memif->use_adsp_share_mem);
 
-		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value)
+		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value) {
+			memif->err_close_order = true;
 			AUDIO_AEE("disable adsp memory used before substream shutdown");
+		}
 	}
 
 	if (ul_memif_num >= 0) {
@@ -1300,8 +1307,10 @@ static int mt6989_adsp_mem_set(struct snd_kcontrol *kcontrol,
 		pr_info("%s(), ul:%s, use_adsp_share_mem %d->%d\n",
 			__func__, memif->data->name, old_value, memif->use_adsp_share_mem);
 
-		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value)
+		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value) {
+			memif->err_close_order = true;
 			AUDIO_AEE("disable adsp memory used before substream shutdown");
+		}
 	}
 
 	if (ref_memif_num >= 0) {
@@ -1312,8 +1321,10 @@ static int mt6989_adsp_mem_set(struct snd_kcontrol *kcontrol,
 		pr_info("%s(), ref:%s, use_adsp_share_mem %d->%d\n",
 			__func__, memif->data->name, old_value, memif->use_adsp_share_mem);
 
-		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value)
+		if (memif->substream && memif->use_adsp_share_mem == 0 && old_value) {
+			memif->err_close_order = true;
 			AUDIO_AEE("disable adsp memory used before substream shutdown");
+		}
 	}
 
 	return 0;
