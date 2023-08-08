a86a52dee5f954c9a5fc191fe8defa58062b61ec
Walter-zh Wu <walter-zh.wu@mediatek.com>
2023-08-08 13:51:55 +0800

    [ALPS08272181] tmem: fix coverity issue
    fix the uninitialized scalar variable issue
    
    MTK-Commit-Id: 0de18c0871dea11286e6dc79a1a0b19cce0d61e9
    
    Signed-off-by: Walter-zh Wu <walter-zh.wu@mediatek.com>
    CR-Id: ALPS08272181
    Feature: devmpu
    Change-Id: Id06eae5ca64933a8a767a554c905b0e735bfb295

--- drivers/misc/mediatek/trusted_mem/tests/ut_ssheap.c
+++ drivers/misc/mediatek/trusted_mem/tests/ut_ssheap.c
@@ -188,7 +188,7 @@ out:
 static ssize_t ssheap_write(struct file *file, const char __user *buffer,
 			    size_t count, loff_t *data)
 {
-	char desc[128];
+	char desc[128] = {0};
 
 	if (copy_from_user(desc, buffer, count))
 		return 0;
