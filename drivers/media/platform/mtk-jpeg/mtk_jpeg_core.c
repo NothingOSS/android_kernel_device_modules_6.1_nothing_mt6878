f1419bad94d67fc7e77c176b2a991bdd0de699cb
Sean Lin <yu-syuan.Lin@mediatek.com>
2023-08-08 13:51:15 +0800

    [ALPS08258447] jpeg: adjust afbc buffer formula
    header size align 1024
    
    MTK-Commit-Id: 93741d65671142163f93d1196f3ccc03252fa090
    
    Signed-off-by: Sean Lin <yu-syuan.Lin@mediatek.com>
    CR-Id: ALPS08258447
    Feature: Video Driver
    Change-Id: I014a024a197e2c6bc8dec4d149486df9aaf44617

--- drivers/media/platform/mtk-jpeg/mtk_jpeg_core.c
+++ drivers/media/platform/mtk-jpeg/mtk_jpeg_core.c
@@ -318,14 +318,14 @@ static int mtk_jpeg_try_fmt_mplane(struct v4l2_pix_format_mplane *pix_mp,
 			unsigned int block_w = 16;
 			unsigned int block_h = 16;
 			unsigned int bitsPP = 12;
-			unsigned int width = round_up(pix_mp->width, 32);
-			unsigned int height = round_up(pix_mp->height, 32);
+			unsigned int width = round_up(pix_mp->width, 16);
+			unsigned int height = round_up(pix_mp->height, 16);
 			unsigned int block_count =
 					((width + (block_w - 1))/block_w)
 					*((height + (block_h - 1))/block_h);
 			pfmt->bytesperline = stride;
-			pfmt->sizeimage = (block_count << 4) +
-					(block_count * block_w * block_h  * bitsPP / 8);
+			pfmt->sizeimage = round_up((block_count << 4), 1024) +
+					(block_count * round_up((block_w * block_h  * bitsPP / 8), 128));
 		} else {
 			pfmt->bytesperline = stride;
 			pfmt->sizeimage = stride * h;
