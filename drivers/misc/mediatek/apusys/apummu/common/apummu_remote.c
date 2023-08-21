86449299380ec641a93718d5f57ecb4884d97ab0
Allan Chia <allan.chia@mediatek.com>
2023-08-21 13:37:20 +0800

    [ALPS08280671] apummu: set ipi retry time longer
    1. Set IPI retry longer to prevent seldom worst case,
    send IPI when APU RV is in deep idle flow
    
    MTK-Commit-Id: 96f4e413e2f91dc8c095d0057cb5ab0a9259d1bd
    
    Signed-off-by: Allan Chia <allan.chia@mediatek.com>
    CR-Id: ALPS08280671
    Feature: apu_sys_kernel
    Change-Id: Iccdeccb47a9a1270ec0a333f902d90d41c8b991b

--- drivers/misc/mediatek/apusys/apummu/common/apummu_remote.c
+++ drivers/misc/mediatek/apusys/apummu/common/apummu_remote.c
@@ -73,7 +73,7 @@ int apummu_remote_send_cmd_sync(void *drvinfo, void *request, void *reply, uint3
 	int retry = 0;
 	bool find = false;
 	uint32_t *ptr;
-	uint32_t cnt = 40, i = 0;
+	uint32_t cnt = 200, i = 0;
 
 	if (drvinfo == NULL) {
 		AMMU_LOG_ERR("invalid argument\n");
@@ -111,6 +111,8 @@ int apummu_remote_send_cmd_sync(void *drvinfo, void *request, void *reply, uint3
 				usleep_range(200, 500);
 			else if (ret == -EAGAIN && i < 50)
 				usleep_range(1000, 2000);
+			else if (ret == -EAGAIN && i < 200)
+				usleep_range(10000, 20000);
 			continue;
 		}
 		break;
