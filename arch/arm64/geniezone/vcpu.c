05dcbcce6f5e558dd900a4deefe71243784da64e
Willix Yeh <chi-shen.yeh@mediatek.com>
2023-08-21 13:38:59 +0800

    [ALPS08292010] AVF: hrtimer expire function
    [Detail]
    - remove hrtimer related function.
    - remove ppi injection when hrtimer expired.
    - schedule back to guest vm, and timer interrupt
      will be triggered automatically.
    
    MTK-Commit-Id: 5ac272e4e91d65a5d27fa4f647505023247258e6
    
    CR-Id: ALPS08292010
    Feature: Android Virtualization Framework
    Change-Id: I4b5968c108efab6314306efc538eb26b2013955a
    Signed-off-by: Willix Yeh <chi-shen.yeh@mediatek.com>

--- arch/arm64/geniezone/vcpu.c
+++ arch/arm64/geniezone/vcpu.c
@@ -41,87 +41,15 @@ int gzvm_arch_vcpu_update_one_reg(struct gzvm_vcpu *vcpu, __u64 reg_id,
 	return ret;
 }
 
-static void clear_migrate_state(struct gzvm_vcpu *vcpu)
-{
-	vcpu->hwstate->vtimer_migrate = 0;
-	vcpu->hwstate->vtimer_delay = 0;
-}
-
-static u64 gzvm_mtimer_delay_time(u64 delay)
-{
-	u64 ns;
-
-	ns = clocksource_cyc2ns(delay, clock_scale_factor.mult,
-				clock_scale_factor.shift);
-
-	return ns;
-}
-
-static void gzvm_mtimer_release(struct gzvm_vcpu *vcpu)
-{
-	hrtimer_cancel(&vcpu->gzvm_mtimer);
-
-	clear_migrate_state(vcpu);
-}
-
-static void gzvm_mtimer_catch(struct hrtimer *hrt, u64 delay)
-{
-	u64 ns;
-
-	ns = gzvm_mtimer_delay_time(delay);
-	hrtimer_start(hrt, ktime_add_ns(ktime_get(), ns), HRTIMER_MODE_ABS_HARD);
-}
-
-static void mtimer_irq_forward(struct gzvm_vcpu *vcpu)
-{
-	struct gzvm *gzvm;
-	u32 irq_num, vcpu_idx, vcpu2_idx;
-
-	gzvm = vcpu->gzvm;
-
-	irq_num = FIELD_GET(GZVM_IRQ_LINE_NUM, GZVM_VTIMER_IRQ);
-	vcpu_idx = FIELD_GET(GZVM_IRQ_LINE_VCPU, GZVM_VTIMER_IRQ);
-	vcpu2_idx = FIELD_GET(GZVM_IRQ_LINE_VCPU2, GZVM_VTIMER_IRQ) *
-		    (GZVM_IRQ_VCPU_MASK + 1);
-
-	gzvm_vgic_inject_ppi(gzvm, vcpu_idx + vcpu2_idx, irq_num, 1);
-}
-
-static enum hrtimer_restart gzvm_mtimer_expire(struct hrtimer *hrt)
-{
-	struct gzvm_vcpu *vcpu;
-
-	vcpu = container_of(hrt, struct gzvm_vcpu, gzvm_mtimer);
-
-	mtimer_irq_forward(vcpu);
-
-	return HRTIMER_NORESTART;
-}
-
-static void vtimer_init(struct gzvm_vcpu *vcpu)
-{
-	/* gzvm_mtimer init based on hrtimer */
-	hrtimer_init(&vcpu->gzvm_mtimer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS_HARD);
-	vcpu->gzvm_mtimer.function = gzvm_mtimer_expire;
-}
-
 int gzvm_arch_vcpu_run(struct gzvm_vcpu *vcpu, __u64 *exit_reason)
 {
 	struct arm_smccc_res res;
 	unsigned long a1;
 	int ret;
 
-	/* hrtimer cancel and clear migrate state */
-	if (vcpu->hwstate->vtimer_migrate)
-		gzvm_mtimer_release(vcpu);
-
 	a1 = assemble_vm_vcpu_tuple(vcpu->gzvm->vm_id, vcpu->vcpuid);
 	ret = gzvm_hypcall_wrapper(MT_HVC_GZVM_RUN, a1, 0, 0, 0, 0, 0,
 				   0, &res);
-
-	/* hrtimer register if migration needed */
-	if (vcpu->hwstate->vtimer_migrate)
-		gzvm_mtimer_catch(&vcpu->gzvm_mtimer, vcpu->hwstate->vtimer_delay);
 
 	*exit_reason = res.a1;
 	return ret;
@@ -131,8 +59,6 @@ int gzvm_arch_destroy_vcpu(struct gzvm_vcpu *vcpu)
 {
 	struct arm_smccc_res res;
 	unsigned long a1;
-
-	hrtimer_cancel(&vcpu->gzvm_mtimer);
 
 	a1 = assemble_vm_vcpu_tuple(vcpu->gzvm->vm_id, vcpu->vcpuid);
 	gzvm_hypcall_wrapper(MT_HVC_GZVM_DESTROY_VCPU, a1, 0, 0, 0, 0, 0, 0,
@@ -152,8 +78,6 @@ int gzvm_arch_create_vcpu(struct gzvm_vcpu *vcpu)
 	struct arm_smccc_res res;
 	unsigned long a1, a2;
 	int ret;
-
-	vtimer_init(vcpu);
 
 	a1 = assemble_vm_vcpu_tuple(vcpu->gzvm->vm_id, vcpu->vcpuid);
 	a2 = (__u64)virt_to_phys(vcpu->run);
