2a996586b22563514fe71f8eadc5c814552f2855
Scott Wang <scott.wang@mediatek.com>
2023-09-04 15:42:41 +0800

    [ALPS08282720] disp: correct mtk_crtc_all_layer_off()
    [Detail]
    mtk_crtc_all_layer_off() should keep the first layer on instead of
    keep each OVLs first layer on
    
    MTK-Commit-Id: 40348fa697b3673f7ed68fb26433feb510b0f6d8
    
    CR-Id: ALPS08282720
    Feature: display_mobile
    Signed-off-by: Scott Wang <scott.wang@mediatek.com>
    Change-Id: I2b469651f4f2c2b555da2c70048e5e0e2130db16

--- drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mediatek_v2/mtk_drm_crtc.c
@@ -10306,14 +10306,19 @@ static void mtk_crtc_all_layer_off(struct mtk_drm_crtc *mtk_crtc,
 	struct mtk_ddp_comp *comp;
 
 	keep_first_layer = true;
-	for_each_comp_in_cur_crtc_path(comp, mtk_crtc, i, j)
+	for_each_comp_in_cur_crtc_path(comp, mtk_crtc, i, j) {
 		mtk_ddp_comp_io_cmd(comp, cmdq_handle,
 			OVL_ALL_LAYER_OFF, &keep_first_layer);
+		keep_first_layer = false;
+	}
 
 	if (mtk_crtc->is_dual_pipe) {
-		for_each_comp_in_dual_pipe(comp, mtk_crtc, i, j)
+		keep_first_layer = true;
+		for_each_comp_in_dual_pipe(comp, mtk_crtc, i, j) {
 			mtk_ddp_comp_io_cmd(comp, cmdq_handle,
 				OVL_ALL_LAYER_OFF, &keep_first_layer);
+			keep_first_layer = false;
+		}
 	}
 }
 
