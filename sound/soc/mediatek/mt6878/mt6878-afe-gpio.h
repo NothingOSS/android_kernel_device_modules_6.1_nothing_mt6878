f655ecd910e1eee9951ca7eee42932b6ae607b29
shu-wei.hsu <shu-wei.hsu@mediatek.com>
2023-09-27 20:54:55 +0800

    [ALPS08277661] ASoC: update audio driver setting
    [Detail]
    Update audio gpio/clk setting
    
    MTK-Commit-Id: 710a7be2b0d568c7525fb5b9685dae3a9ba33879
    
    Signed-off-by: shu-wei.hsu <shu-wei.hsu@mediatek.com>
    CR-Id: ALPS08277661
    Feature: alsa_sound_soc
    Change-Id: I3421a2b250ffa8468502e2c9af368a006b529fb0

---
+++ sound/soc/mediatek/mt6878/mt6878-afe-gpio.h
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * mt6878-afe-gpio.h  --  Mediatek 6878 afe gpio ctrl definition
+ *
+ *  Copyright (c) 2023 MediaTek Inc.
+ *  Author: Shu-wei Hsu <Shu-wei Hsu@mediatek.com>
+ */
+
+#ifndef _MT6878_AFE_GPIO_H_
+#define _MT6878_AFE_GPIO_H_
+
+enum mt6878_afe_gpio {
+	MT6878_AFE_GPIO_DAT_MISO0_OFF,
+	MT6878_AFE_GPIO_DAT_MISO0_ON,
+	MT6878_AFE_GPIO_DAT_MISO1_OFF,
+	MT6878_AFE_GPIO_DAT_MISO1_ON,
+	MT6878_AFE_GPIO_DAT_MOSI_OFF,
+	MT6878_AFE_GPIO_DAT_MOSI_ON,
+	MT6878_AFE_GPIO_I2SIN4_OFF,
+	MT6878_AFE_GPIO_I2SIN4_ON,
+	MT6878_AFE_GPIO_I2SOUT4_OFF,
+	MT6878_AFE_GPIO_I2SOUT4_ON,
+	MT6878_AFE_GPIO_VOW_SCP_DMIC_DAT_OFF,
+	MT6878_AFE_GPIO_VOW_SCP_DMIC_DAT_ON,
+	MT6878_AFE_GPIO_VOW_SCP_DMIC_CLK_OFF,
+	MT6878_AFE_GPIO_VOW_SCP_DMIC_CLK_ON,
+	MT6878_AFE_GPIO_GPIO_NUM
+};
+
+struct mtk_base_afe;
+
+int mt6878_afe_gpio_init(struct mtk_base_afe *afe);
+int mt6878_afe_gpio_request(struct mtk_base_afe *afe, bool enable,
+			    int dai, int uplink);
+bool mt6878_afe_gpio_is_prepared(enum mt6878_afe_gpio type);
+
+#endif
