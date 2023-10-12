a6748c893e783554ae4a1412daf01e0981f46cbd
Jasper Yen <jasper.yen@mediatek.com>
2023-10-12 14:48:41 +0800

    [ALPS08344778] apusys_rv: enable preload uP FW from lk2
    1. Enable preload uP FW from lk2
    
    MTK-Commit-Id: 6bdfc9e9ec699160a3299bfff5b4d845214ae216
    
    CR-Id: ALPS08344778
    Feature: apu_microp_kernel
    Change-Id: I6df8d276e6cda2affcba0fa40437f0aeee285611
    Signed-off-by: Jasper Yen <jasper.yen@mediatek.com>

--- drivers/misc/mediatek/apusys/apusys_rv/2.0/mt6878_plat.c
+++ drivers/misc/mediatek/apusys/apusys_rv/2.0/mt6878_plat.c
@@ -716,7 +716,7 @@ static void mt6878_rv_cachedump(struct mtk_apu *apu)
 }
 
 const struct mtk_apu_platdata mt6878_platdata = {
-	.flags		= F_AUTO_BOOT | F_DEBUG_LOG_ON,
+	.flags		= F_PRELOAD_FIRMWARE | F_AUTO_BOOT | F_DEBUG_LOG_ON,
 	.ops		= {
 		.init	= mt6878_rproc_init,
 		.exit	= mt6878_rproc_exit,
