3676f8390c32e3691b971002dae60920f78891bd
barry.wang <barry.wang@mediatek.com>
2023-10-31 10:05:18 +0800

    [ALPS08224617] iommu/mediatek: mt6878: support dynamic power control
    1. Remove IOMMU_CLK_AO_EN flag.
    2. Enable device link flag LINK_WITH_APU for mt6878 apu power control.
    
    MTK-Commit-Id: 96ce35e1a9b16cee395be0e2efe4934ac77a6a87
    
    CR-Id: ALPS08224617
    Feature: iommu_kernel
    Signed-off-by: barry.wang <barry.wang@mediatek.com>
    Change-Id: I6de44b48cd3cd4628ebeed3cccebd1a6eb2214c7

--- drivers/iommu/mtk_iommu.c
+++ drivers/iommu/mtk_iommu.c
@@ -3454,7 +3454,6 @@ static const struct mtk_iommu_plat_data mt6878_data_disp = {
 	.flags          = OUT_ORDER_WR_EN | GET_DOM_ID_LEGACY |
 			  NOT_STD_AXI_MODE | TLB_SYNC_EN | IOMMU_SEC_EN |
 			  SKIP_CFG_PORT | IOVA_34_EN | PGTABLE_PA_35_EN |
-			  IOMMU_CLK_AO_EN | /*HAS_BCLK |*/
 			  HAS_SMI_SUB_COMM | SAME_SUBSYS,
 	.hw_list        = &mm_iommu_list,
 	.inv_sel_reg    = REG_MMU_INV_SEL_GEN2,
@@ -3470,8 +3469,8 @@ static const struct mtk_iommu_plat_data mt6878_data_disp = {
 static const struct mtk_iommu_plat_data mt6878_data_apu0 = {
 	.m4u_plat	= M4U_MT6878,
 	.flags          = TLB_SYNC_EN | IOMMU_SEC_EN | PGTABLE_PA_35_EN |
-			  GET_DOM_ID_LEGACY | IOVA_34_EN | /*LINK_WITH_APU |*/
-			  PM_OPS_SKIP | IOMMU_CLK_AO_EN,
+			  GET_DOM_ID_LEGACY | IOVA_34_EN | LINK_WITH_APU |
+			  PM_OPS_SKIP,
 	.hw_list        = &apu_iommu_list,
 	.inv_sel_reg    = REG_MMU_INV_SEL_GEN2,
 	.iommu_id	= APU_IOMMU0,
